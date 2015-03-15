dfce6f339626e0f86ece17c162c371ad0a43f438
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-03-15 16:41:10 +0100

    GUI adapting to OpenGL changes
    TODO: Test GUI features and most likely make changes

--- gui/Background.cpp
+++ gui/Background.cpp
@@ -5,21 +5,21 @@
 using namespace L;
 using namespace GUI;
 
-Background::Background(const Ref<Base>& inner, Color c) : Layer(inner), color(c){}
+Background::Background(const Ref<Base>& inner, Color c) : Layer(inner), color(c) {}
 
-void Background::draw(){
-    Interval2i clip(gClip());
-    std::cout << clip.gA().x() << " " << clip.gA().y() << " "
-              << clip.gB().x() << " " << clip.gB().y() << std::endl;
-    if(!clip.empty() || true){
-        glColor3ub(color.r(),color.g(),color.b());
-        glBegin(GL_QUADS);
-            glVertex2i(clip.gA().x(),clip.gA().y());
-            glVertex2i(clip.gB().x(),clip.gA().y());
-            glVertex2i(clip.gB().x(),clip.gB().y());
-            glVertex2i(clip.gA().x(),clip.gB().y());
-        glEnd();
-        inner->draw();
-    }
+void Background::draw() {
+  Interval2i clip(gClip());
+  std::cout << clip.min().x() << " " << clip.min().y() << " "
+            << clip.max().x() << " " << clip.max().y() << std::endl;
+  if(!clip.empty() || true) {
+    glColor3ub(color.r(),color.g(),color.b());
+    glBegin(GL_QUADS);
+    glVertex2i(clip.min().x(),clip.min().y());
+    glVertex2i(clip.max().x(),clip.min().y());
+    glVertex2i(clip.max().x(),clip.max().y());
+    glVertex2i(clip.min().x(),clip.max().y());
+    glEnd();
+    inner->draw();
+  }
 }
 
--- gui/Image.cpp
+++ gui/Image.cpp
@@ -7,41 +7,39 @@ using namespace GUI;
 using L::Image::Bitmap;
 using GUI::Image;
 
-GUI::Image::Image() : Solid(), dimensions(0,0){}
-GUI::Image::Image(const Bitmap& bmp) : Solid(){
-    sBitmap(bmp);
+GUI::Image::Image() : Solid(), dimensions(0,0) {}
+GUI::Image::Image(const Bitmap& bmp) : Solid() {
+  sBitmap(bmp);
 }
-void GUI::Image::sBitmap(const Bitmap& bmp){
-    tex = new GL::Texture(bmp);
-    dimensions.x() = bmp.width();
-    dimensions.y() = bmp.height();
-    dimensionsChanged(dimensions);
+void GUI::Image::sBitmap(const Bitmap& bmp) {
+  tex = new GL::Texture(bmp);
+  dimensions.x() = bmp.width();
+  dimensions.y() = bmp.height();
+  dimensionsChanged(dimensions);
 }
 
-Point2i GUI::Image::gDimensions(){
-    return dimensions;
+Point2i GUI::Image::gDimensions() {
+  return dimensions;
 }
-void GUI::Image::draw(){
-    if(tex && !clip.empty()){
-        Vector<float> vertex(8), texCoord(8);
-
-        vertex[0] = clip.gA().x();
-        vertex[1] = clip.gA().y();
-        vertex[2] = clip.gB().x();
-        vertex[3] = clip.gA().y();
-        vertex[4] = clip.gB().x();
-        vertex[5] = clip.gB().y();
-        vertex[6] = clip.gA().x();
-        vertex[7] = clip.gB().y();
-
-        texCoord[0] = (clip.gA().x()>pos.x()) ? (float)(clip.gA().x()-pos.x())/dimensions.x() : 0.0;
-        texCoord[1] = (clip.gA().y()>pos.y()) ? (float)(clip.gA().y()-pos.y())/dimensions.y() : 0.0;
-        texCoord[2] = (clip.gB().x()<pos.x()+dimensions.x()) ? (float)(clip.gB().x()-pos.x())/dimensions.x() : 1.0;
-        texCoord[3] = (clip.gA().y()>pos.y()) ? (float)(clip.gA().y()-pos.y())/dimensions.y() : 0.0;
-        texCoord[4] = (clip.gB().x()<pos.x()+dimensions.x()) ? (float)(clip.gB().x()-pos.x())/dimensions.x() : 1.0;
-        texCoord[5] = (clip.gB().y()<pos.y()+dimensions.y()) ? (float)(clip.gB().y()-pos.y())/dimensions.y() : 1.0;
-        texCoord[6] = (clip.gA().x()>pos.x()) ? (float)(clip.gA().x()-pos.x())/dimensions.x() : 0.0;
-        texCoord[7] = (clip.gB().y()<pos.y()+dimensions.y()) ? (float)(clip.gB().y()-pos.y())/dimensions.y() : 1.0;
-        GL::Utils::draw2dTexQuad(vertex,texCoord,*tex);
-    }
+void GUI::Image::draw() {
+  if(tex && !clip.empty()) {
+    Vector<float> vertex(8), texCoord(8);
+    vertex[0] = clip.min().x();
+    vertex[1] = clip.min().y();
+    vertex[2] = clip.max().x();
+    vertex[3] = clip.min().y();
+    vertex[4] = clip.max().x();
+    vertex[5] = clip.max().y();
+    vertex[6] = clip.min().x();
+    vertex[7] = clip.max().y();
+    texCoord[0] = (clip.min().x()>pos.x()) ? (float)(clip.min().x()-pos.x())/dimensions.x() : 0.0;
+    texCoord[1] = (clip.min().y()>pos.y()) ? (float)(clip.min().y()-pos.y())/dimensions.y() : 0.0;
+    texCoord[2] = (clip.max().x()<pos.x()+dimensions.x()) ? (float)(clip.max().x()-pos.x())/dimensions.x() : 1.0;
+    texCoord[3] = (clip.min().y()>pos.y()) ? (float)(clip.min().y()-pos.y())/dimensions.y() : 0.0;
+    texCoord[4] = (clip.max().x()<pos.x()+dimensions.x()) ? (float)(clip.max().x()-pos.x())/dimensions.x() : 1.0;
+    texCoord[5] = (clip.max().y()<pos.y()+dimensions.y()) ? (float)(clip.max().y()-pos.y())/dimensions.y() : 1.0;
+    texCoord[6] = (clip.min().x()>pos.x()) ? (float)(clip.min().x()-pos.x())/dimensions.x() : 0.0;
+    texCoord[7] = (clip.max().y()<pos.y()+dimensions.y()) ? (float)(clip.max().y()-pos.y())/dimensions.y() : 1.0;
+    GL::Utils::draw2dTexQuad(vertex,texCoord,*tex);
+  }
 }
