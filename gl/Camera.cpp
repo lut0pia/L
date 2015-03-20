3bcd67ce39aa3a4ce1dda97c78eab13d1b230668
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-03-20 01:22:01 +0100

    Perspective matrix and screenToRay method

--- gl/Camera.cpp
+++ gl/Camera.cpp
@@ -6,8 +6,9 @@
 using namespace L;
 using namespace GL;
 
-Camera::Camera(const Point3f& position, float fovy, float aspect, float nearLimit, float farLimit)
-  : _position(position), _lookat(position+Point3f(0,0,-1)), _up(0,1,0), _fovy(fovy), _aspect(aspect), _nearLimit(nearLimit), _farLimit(farLimit) {
+Camera::Camera(const Point3f& position)
+  : _position(position), _lookat(position+Point3f(0,0,-1)), _up(0,1,0) {
+  perspective(60,16.0/9.0,.01,512);
   update();
 }
 
@@ -38,27 +39,34 @@ void Camera::theta(float angle) {
   _lookat -= _position;
   _lookat = Matrix33f::rotation(_right,angle) * _lookat;
   _lookat += _position;
+  update();
 }
 
-void Camera::fovy(float f) {
-  _fovy = f;
-  update();
+void Camera::perspective(float fovy, float aspect, float near, float far) {
+  _fovy = fovy;
+  _aspect = aspect;
+  _near = near;
+  _far = far;
+  _projection = Matrix44f::identity();
+  float top(near*tan(fovy*(M_PI/360))), right(top*aspect);
+  _projection(0,0) = near/right;
+  _projection(1,1) = near/top;
+  _projection(2,2) = (-far+near)/(far-near);
+  _projection(2,3) = (-2*far*near)/(far-near);
+  _projection(3,2) = -1;
+  _projection(3,3) = 0;
 }
-void Camera::aspect(float a) {
-  _aspect = a;
-  update();
+void Camera::ortho(float left, float right, float bottom, float top, float near, float far) {
 }
-void Camera::clipping(float n, float f) {
-  _nearLimit = n;
-  _farLimit = f;
-  update();
+void Camera::pixels() {
+  //ortho(0,Window::width(),0,Window::height());
+}
+Point3f Camera::screenToRay(const Point2f& p) const {
+  float fovx(_aspect*_fovy);
+  return Matrix33f::rotation(_up,-fovx*(M_PI/360)*p.x()) * Matrix33f::rotation(_right,_fovy*(M_PI/360)*p.y()) * _forward;
 }
 
 void Camera::update() {
-  // Communicate perspective information to OpenGL
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(_fovy,_aspect,_nearLimit,_farLimit);
   // Compute new vectors
   _forward = _lookat - _position;
   _forward.normalize();
@@ -68,11 +76,5 @@ void Camera::update() {
   _up.normalize();
   // Compute new view matrix
   _view = Matrix44f::orientation(_right,_up,-_forward).transpose() * Matrix44f::translation(-_position);
-}
-void Camera::place() {
-  glMatrixMode(GL_MODELVIEW);
-  //gluLookAt(_position.x(), _position.y(), _position.z(), _lookat.x(), _lookat.y(), _lookat.z(), _up.x(), _up.y(), _up.z());
-  //glLoadTransposeMatrixf(_view.array());
-  glLoadIdentity();
 }
 
--- gl/Camera.h
+++ gl/Camera.h
@@ -10,28 +10,26 @@ namespace L {
       protected:
         Point3f _position, _lookat, _up, _forward, _right;
         Matrix44f _view, _projection;
-        float _fovy, _aspect, _nearLimit, _farLimit;
+        float _fovy, _aspect, _near, _far;
 
       public:
-        Camera(const Point3f& position, float fovy = 60, float aspect = 16.0/9.0, float nearLimit = .01, float farLimit = 512);
+        Camera(const Point3f& position);
 
         void move(const Point3f& delta);
         void position(const Point3f& position);
-        const Point3f& position() const{return _position;}
-        const Point3f& forward() const{return _forward;}
         void lookat(const Point3f&);
 
         void phi(float);
         void theta(float);
 
-        void fovy(float fovy);
-        void aspect(float aspect);
-        void clipping(float near, float far);
-        void perspective(float fov, float aspect, float near, float far);
-        void ortho();
+        void perspective(float fovy, float aspect, float near, float far);
+        void ortho(float left, float right, float bottom, float top, float near = -1, float far = 1);
+        void pixels();
+        Point3f screenToRay(const Point2f&) const;
 
         void update();
-        void place();
+        const Point3f& position() const{return _position;}
+        const Point3f& forward() const{return _forward;}
         const Matrix44f& view() {return _view;}
         const Matrix44f& projection() {return _projection;}
     };
