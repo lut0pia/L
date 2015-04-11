a537862fb6c334cae1a48bde2893f7cbfa0b0802
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-04-11 11:34:29 +0200

    Fixed Exception
    Since apparently I can't get MSVC to display exceptions' what, I'm
    simply outing them before they're thrown, it's always better than a
    simple 0xc0000409 return value (unkwown software exception).

--- Exception.cpp
+++ Exception.cpp
@@ -2,7 +2,9 @@
 
 using namespace L;
 
-Exception::Exception(const String& w) : w(w) {}
+Exception::Exception(const String& w) : w(w) {
+  std::cout << w << std::endl;
+}
 Exception::~Exception() throw() {}
 const char* Exception::what() const throw() {
   return w.c_str();
