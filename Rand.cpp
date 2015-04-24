278ab1d133681121910b3fb1866f0da043064d51
Lyatus (Box) <lutopia@hotmail.fr>
2015-04-24 14:36:14 +0200

    Added Rand::nextInt

--- Rand.cpp
+++ Rand.cpp
@@ -9,37 +9,39 @@ using namespace L;
 const ullong Rand::a(2862933555777941757LL), Rand::b(3037000493LL);
 ullong Rand::last(time(NULL));
 
-ullong Rand::next(){
-    return last = last*a+b;
+ullong Rand::next() {
+  return last = last*a+b;
 }
-byte Rand::nextByte(){
-    return next()>>48;
+int Rand::nextInt() {
+  return next()>>32;
 }
-float Rand::nextFloat(){
-    return next()/(float)(ullong)-1;
+byte Rand::nextByte() {
+  return next()>>48;
 }
-
-ullong Rand::next(ullong min, ullong max){
-    return (next()%(max-min+1))+min;
-}
-int Rand::next(int min, int max){
-    return ((next()>>32)%(max-min+1))+min;
-}
-float Rand::next(float min, float max){
-    return (nextFloat()*(max-min))+min;
+float Rand::nextFloat() {
+  return next()/(float)(ullong)-1;
 }
 
-float Rand::gauss(uint i){
-    float wtr(0);
-    for(uint j(0);j<i;j++)
-        wtr += nextFloat();
-    return wtr/i;
+ullong Rand::next(ullong min, ullong max) {
+  return (next()%(max-min+1))+min;
 }
-ullong Rand::next(ullong ave){
-    if(ave){
-        ullong wtr(0), thres(((ullong)-1)/(ave+1));
-        while(next()>thres) wtr++;
-        return wtr;
-    }
-    else return 0;
+int Rand::next(int min, int max) {
+  return ((next()>>32)%(max-min+1))+min;
+}
+float Rand::next(float min, float max) {
+  return (nextFloat()*(max-min))+min;
+}
+
+float Rand::gauss(uint i) {
+  float wtr(0);
+  for(uint j(0); j<i; j++)
+    wtr += nextFloat();
+  return wtr/i;
+}
+ullong Rand::next(ullong ave) {
+  if(ave) {
+    ullong wtr(0), thres(((ullong)-1)/(ave+1));
+    while(next()>thres) wtr++;
+    return wtr;
+  } else return 0;
 }
--- Rand.h
+++ Rand.h
@@ -3,27 +3,28 @@
 
 #include "types.h"
 
-namespace L{
-    class Rand{
-        private:
-            static const ullong a, b;
-            static ullong last;
+namespace L {
+  class Rand {
+    private:
+      static const ullong a, b;
+      static ullong last;
 
-        public:
-            static ullong next();
-            static byte nextByte();
-            static float nextFloat();
+    public:
+      static ullong next();
+      static byte nextByte();
+      static int nextInt();
+      static float nextFloat();
 
-            static ullong next(ullong min, ullong max); // Returns a random unsigned integer between min and max
-            static int next(int min, int max); // Returns a random integer between min and max
-            static float next(float min, float max); // Returns a random float between min and max
+      static ullong next(ullong min, ullong max); // Returns a random unsigned integer between min and max
+      static int next(int min, int max); // Returns a random integer between min and max
+      static float next(float min, float max); // Returns a random float between min and max
 
-            static float gauss(uint);
-            static ullong next(ullong ave); // Random exp with an average n
+      static float gauss(uint);
+      static ullong next(ullong ave); // Random exp with an average n
 
-            inline static const ullong& gState(){return last;}
-            inline static void sState(const ullong& s){last = s;}
-    };
+      inline static const ullong& gState() {return last;}
+      inline static void sState(const ullong& s) {last = s;}
+  };
 }
 
 #endif
