c7c9e169211148fc5ca0470da61434944a2a0b62
Lyatus (Box) <lutopia@hotmail.fr>
2015-04-28 19:46:07 +0200

    Improved NNS in Tree

--- containers/Tree.h
+++ containers/Tree.h
@@ -28,7 +28,7 @@ namespace L {
           inline const Key& key() const {return _key;}
           inline const V& value() const {return _value;}
 
-          int childIndex(const Key& key) const { // Returns the child index, returns -1 when both keys are equal
+          int childIndex(const Key& key) const { // Returns the child index for a key, returns -1 when both keys are equal
             int eq(0), wtr(0);
             for(int i(0); i<d; i++) {
               wtr <<= 1;
@@ -39,6 +39,9 @@ namespace L {
           }
           inline static int oppositeIndex(int index) {
             return (~index)&mask;
+          }
+          inline static int mirrorIndex(int index, int components) {
+            return (index^components)&mask;
           }
 
           static Node* insert(Node*& node, const Key& key, const V& value) {
@@ -57,25 +60,38 @@ namespace L {
               return (child>=0) ? find(node->_children[child],key) : node;
             } else return NULL;
           }
+          static void nearestCmp(const Node*& currentNode, K& currentDistance, const Node* newNode, const Key& key) {
+            if(newNode) {
+              K newDistance(newNode->key().distSquared(key));
+              if(newDistance<currentDistance) {
+                currentNode = newNode;
+                currentDistance = newDistance;
+              }
+            }
+          }
           static const Node* nearest(const Node* node, const Key& key) {
+            static int avoided(0), total(0);
             if(node) {
-              K distance(node->key().distSquared(key));
-              int child(node->childIndex(key));
-              if(child<0) return node; // This node has this key (so it's nearest)
-              int oppositeChild(oppositeIndex(child));
-              const Node* wtr(node);
-              for(int i(0); i<n; i++)
-                if(i!=oppositeChild) {
-                  const Node* tmpNode(nearest(node->_children[i],key));
-                  if(tmpNode) {
-                    K tmpDistance(tmpNode->key().distSquared(key));
-                    //std::cout << tmpNode->key() << " ->" << tmpDistance << " (" << distance << ")" << std::endl;
-                    if(tmpDistance<distance) {
-                      wtr = tmpNode;
-                      distance = tmpDistance;
-                    }
-                  }
+              int bestChild(node->childIndex(key));
+              if(bestChild<0) return node; // This node has this key (so it's nearest of all)
+              const Node* wtr(node); // As far as we know this is the nearest
+              K distance(wtr->key().distSquared(key)); // Compute distance from key
+              nearestCmp(wtr,distance,nearest(node->_children[bestChild],key),key);// Have to test with the best child node first
+              for(int i(1); i<n; i++) { // For each possible combination of component mirroring
+                Key nearestPossibleVector(key-node->key()); // Compute minimum vector coming from mirrored best child
+                for(int j(0); j<d; j++)
+                  if(!(i&(1<<j)))
+                    nearestPossibleVector[d-j-1] = 0;
+                K nearestPossibleDistance(nearestPossibleVector.normSquared()); // Compute minimum distance from mirrored best child
+                if(distance>=nearestPossibleDistance) // Check if mirrored best child could return a node nearer than the current one
+                  nearestCmp(wtr,distance,nearest(node->_children[mirrorIndex(bestChild,i)],key),key);
+                else avoided++;
+                total++;
+                if(total==1000000) {
+                  //std::cout << ((float)avoided/total) << std::endl;
+                  avoided = total = 0;
                 }
+              }
               return wtr;
             } else return NULL;
           }
@@ -90,14 +106,23 @@ namespace L {
               return wtr;
             } else return 0;
           }
+          static int size(const Node* node) {
+            if(node) {
+              int wtr(1);
+              for(int i(0); i<n; i++)
+                wtr += size(node->_children[i]);
+              return wtr;
+            } else return 0;
+          }
       };
       Node* _root;
 
     public:
       Tree() : _root(NULL) {}
       ~Tree() {delete _root;}
-      inline Node* insert(const Key& key, const V& value) {
-        return Node::insert(_root,key,value);
+      inline void insert(const Key& key, const V& value) {
+        Node::insert(_root,key,value);
+        //std::cout << ((log(n)/log(size()))/height()) << std::endl;
       }
       inline const Node* find(const Key& key) const {
         return Node::find(_root,key);
@@ -108,6 +133,10 @@ namespace L {
       inline int height() const {
         return Node::height(_root);
       }
+      inline int size() const {
+        return Node::size(_root);
+      }
+
   };
   // Regular trees
   template <class K,class V> class BinaryTree : public Tree<1,K,V> {};
