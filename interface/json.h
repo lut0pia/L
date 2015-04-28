36dbdc80641e711b74745707ae0a32a4f7037093
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-04-28 10:43:32 +0200

    JSON interface compliance
    Also now uses floats instead of doubles, and resets values before
    reading instead of possibly adding to it.

--- interface/json.h
+++ interface/json.h
@@ -5,116 +5,117 @@
 #include <iostream>
 #include <iomanip>
 
-namespace L{
-    class JSON : public Interface<Dynamic::Var>{
-        public: JSON() : Interface("json"){}
+namespace L {
+  class JSON : public Interface<Dynamic::Var> {
+    public:
+      JSON() : Interface("json") {}
 
-        void to(const Dynamic::Var& v, std::ostream& stream){
-            if(v.is<String>())
-                stream << '"' << v.as<String>() << '"';
-            else if(v.is<bool>())
-                stream << std::boolalpha << v.as<bool>();
-            else if(v.is<Dynamic::Node>()){
-                stream << '{';
-                L_Iter(v.as<Dynamic::Node>(),it){
-                    if(it != v.as<Dynamic::Node>().begin())
-                        stream << ',';
-                    stream << '"' << (*it).first << "\":";
-                    to((*it).second,stream);
-                }
-                stream << '}';
+      bool to(const Dynamic::Var& v, std::ostream& stream) {
+        if(v.is<String>())
+          stream << '"' << v.as<String>() << '"';
+        else if(v.is<bool>())
+          stream << std::boolalpha << v.as<bool>();
+        else if(v.is<Dynamic::Node>()) {
+          stream << '{';
+          L_Iter(v.as<Dynamic::Node>(),it) {
+            if(it != v.as<Dynamic::Node>().begin())
+              stream << ',';
+            stream << '"' << (*it).first << "\":";
+            to((*it).second,stream);
+          }
+          stream << '}';
+        } else if(v.is<Dynamic::Array>()) {
+          stream << '[';
+          L_Iter(v.as<Dynamic::Array>(),it) {
+            if(it != v.as<Dynamic::Array>().begin())
+              stream << ',';
+            to((*it),stream);
+          }
+          stream << ']';
+        } else if(v.is<float>()) {
+          float tmp(v.as<float>());
+          stream << std::fixed << std::noshowpoint << std::setprecision(tmp == std::floor(tmp) ? 0 : 5) << v.as<float>();
+        } else throw "Bad JSON output.";
+        return true;
+      }
+      bool from(Dynamic::Var& v, std::istream& stream) {
+        char c;
+        size_t i;
+        String str;
+        nospace(stream);
+        switch(c = stream.get()) {
+          case '"': // String
+            v = String();
+            while((c = stream.get()) != '"') {
+              if(c == '\\' && stream.peek()=='"')
+                v.as<String>() += stream.get();
+              else v.as<String>() += c;
             }
-            else if(v.is<Dynamic::Array>()){
-                stream << '[';
-                L_Iter(v.as<Dynamic::Array>(),it){
-                    if(it != v.as<Dynamic::Array>().begin())
-                        stream << ',';
-                    to((*it),stream);
-                }
-                stream << ']';
+            break;
+          case 't':
+          case 'f': // Bool
+            if(c=='t') { // true
+              v = true;
+              stream.ignore(3);
+            } else { // false
+              v = false;
+              stream.ignore(4);
             }
-            else if(v.is<double>()){
-                double tmp(v.as<double>());
-                stream << std::fixed << std::noshowpoint << std::setprecision(tmp == std::floor(tmp) ? 0 : 5) << v.as<double>();
+            break;
+          case '{':
+            v = Dynamic::Node();
+            while(stream.peek()!='}') {
+              nospace(stream);
+              if(stream.get()!='"') throw Exception("Bad JSON input.");
+              str.clear();
+              while((c = stream.get()) != '"') { // Get name of attribute
+                if(c == '\\' && stream.peek()=='"')
+                  str += stream.get();
+                else str += c;
+              }
+              nospace(stream);
+              if(stream.get()!=':') throw Exception("Bad JSON input.");
+              from(v[str],stream);
+              if(stream.peek()==',')stream.ignore();
             }
-            else throw "Bad JSON output.";
+            stream.ignore();
+            break;
+          case '[': // Array
+            i = 0;
+            v = Dynamic::Array();
+            while(stream.peek()!=']') {
+              from(v[i],stream);
+              if(stream.peek()==',')stream.ignore();
+              i++;
+            }
+            stream.ignore();
+            break;
+          default: // Probably a number
+            v.get<float>();
+            if(1) {
+              bool negative(c=='-'), afterpoint(false);
+              int point(0);
+              if(!negative) v.get<float>() = c - '0';
+              while(c = stream.peek()) {
+                if(c>='0'&&c<='9') {
+                  if(afterpoint)point++;
+                  (v.as<float>() *= 10) += c - '0';
+                } else if(c=='.')
+                  afterpoint = true;
+                else break;
+                stream.ignore();
+              }
+              while(point--)
+                v.as<float>() /= 10;
+              if(negative)
+                v = -v.as<float>();
+            }
+            break;
         }
-        void from(Dynamic::Var& v, std::istream& stream){
-            char c;
-            size_t i;
-            String str;
-            nospace(stream);
-            switch(c = stream.get()){
-                case '"': // String
-                    v.get<String>();
-                    while((c = stream.get()) != '"'){
-                        if(c == '\\' && stream.peek()=='"')
-                             v.as<String>() += stream.get();
-                        else v.as<String>() += c;
-                    }
-                    break;
-                case 't': case 'f': // Bool
-                    if(c=='t'){ // true
-                        v = true;
-                        stream.ignore(3);
-                    }
-                    else{ // false
-                        v = false;
-                        stream.ignore(4);
-                    }
-                    break;
-                case '{':
-                    while(stream.peek()!='}'){
-                        nospace(stream);
-                        if(stream.get()!='"') throw "Bad JSON input.";
-                        str.clear();
-                        while((c = stream.get()) != '"'){ // Get name of attribute
-                            if(c == '\\' && stream.peek()=='"')
-                                 str += stream.get();
-                            else str += c;
-                        }
-                        nospace(stream);
-                        if(stream.get()!=':') throw "Bad JSON input.";
-                        from(v[str],stream);
-                        if(stream.peek()==',')stream.ignore();
-                    }
-                    stream.ignore();
-                    break;
-                case '[': // Array
-                    i = 0;
-                    while(stream.peek()!=']'){
-                        from(v[i],stream);
-                        if(stream.peek()==',')stream.ignore();
-                        i++;
-                    }
-                    stream.ignore();
-                    break;
-                default: // Probably a number
-                    v.get<double>();
-                    ({
-                        bool negative(c=='-'), afterpoint(false);
-                        int point(0);
-                        if(!negative) v.get<double>() = c - '0';
-                        while(c = stream.peek()){
-                            if(c>='0'&&c<='9'){
-                                if(afterpoint)point++;
-                                (v.as<double>() *= 10) += c - '0';
-                            }
-                            else if(c=='.')
-                                afterpoint = true;
-                            else break;
-                            stream.ignore();
-                        }
-                        while(point--)
-                            v.as<double>() /= 10;
-                        if(negative)
-                            v = -v.as<double>();
-                    });
-                    break;
-            }
-            nospace(stream);
-        }
-    };
+        nospace(stream);
+        return true;
+      }
+  };
 }
 
 #endif
