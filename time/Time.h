bc82919d14e19625d428fa690d9eff571141de3a
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-05-10 20:40:15 +0200

    Indentation compliance

--- time/Time.h
+++ time/Time.h
@@ -3,41 +3,41 @@
 
 #include "../stl/String.h"
 
-namespace L{
-    class Time{
-        protected:
-            long long usec;
-        public:
-            Time();
-            Time(long long useconds);
-            Time(long useconds, long mseconds, long seconds = 0, long minutes = 0, long hours = 0, long days = 0);
+namespace L {
+  class Time {
+    protected:
+      long long usec;
+    public:
+      Time();
+      Time(long long useconds);
+      Time(long useconds, long mseconds, long seconds = 0, long minutes = 0, long hours = 0, long days = 0);
 
-            Time operator+(const Time&) const;
-            Time operator-(const Time&) const;
-            Time operator*(const long long&) const;
-            Time operator/(const long long&) const;
-            bool operator==(const Time&) const;
-            bool operator!=(const Time&) const;
-            bool operator>(const Time&) const;
-            bool operator<(const Time&) const;
-            bool operator>=(const Time&) const;
-            bool operator<=(const Time&) const;
-            Time& operator+=(const Time&);
-            Time& operator-=(const Time&);
+      Time operator+(const Time&) const;
+      Time operator-(const Time&) const;
+      Time operator*(const long long&) const;
+      Time operator/(const long long&) const;
+      bool operator==(const Time&) const;
+      bool operator!=(const Time&) const;
+      bool operator>(const Time&) const;
+      bool operator<(const Time&) const;
+      bool operator>=(const Time&) const;
+      bool operator<=(const Time&) const;
+      Time& operator+=(const Time&);
+      Time& operator-=(const Time&);
 
-            inline long days() const{return (long)(usec/86400000000LL);}
-            inline long hours() const{return (long)(usec/3600000000LL);}
-            inline long minutes() const{return (long)(usec/60000000LL);}
-            inline long seconds() const{return (long)(usec/1000000LL);}
-            inline long long milliseconds() const{return usec/1000LL;}
-            inline long long microseconds() const{return usec;}
+      inline long days() const {return (long)(usec/86400000000LL);}
+      inline long hours() const {return (long)(usec/3600000000LL);}
+      inline long minutes() const {return (long)(usec/60000000LL);}
+      inline long seconds() const {return (long)(usec/1000000LL);}
+      inline long long milliseconds() const {return usec/1000LL;}
+      inline long long microseconds() const {return usec;}
 
-            float fSeconds() const;
+      float fSeconds() const;
 
-            static Time now();
-            static float fps();
-            static String format(String, Time = now());
-    };
+      static Time now();
+      static float fps();
+      static String format(String, Time = now());
+  };
 }
 
 #endif
