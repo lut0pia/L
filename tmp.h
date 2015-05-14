0caedf1b2dec66b2c186b070a4f979edf433b64c
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-05-14 13:21:34 +0200

    Compliance for tmp and added align

--- tmp.h
+++ tmp.h
@@ -3,38 +3,43 @@
 
 #include "types.h"
 
-namespace L{
-    // ENABLE IF
-        template <bool B, class T = void>
-        struct enable_if_c{
-            typedef T type;
-        };
+namespace L {
+  // ENABLE IF
+  template <bool B, class T = void>
+  struct enable_if_c {
+    typedef T type;
+  };
 
-        template <class T>
-        struct enable_if_c<false, T>{};
+  template <class T>
+  struct enable_if_c<false, T> {};
 
-        template <class Cond, class T = void>
-        struct enable_if : public enable_if_c<Cond::value, T>{};
+  template <class Cond, class T = void>
+  struct enable_if : public enable_if_c<Cond::value, T> {};
 
-    // DISABLE IF
-        template <class Cond, class T = void>
-        struct disable_if : public enable_if_c<!Cond::value, T>{};
+  // DISABLE IF
+  template <class Cond, class T = void>
+  struct disable_if : public enable_if_c<!Cond::value, T> {};
 
 
-    template <uint a, uint b>
-    struct static_pow{
-        static const uint value = a*static_pow<a,b-1>::value;
-    };
+  template <uint a, uint b>
+  struct static_pow {
+    static const uint value = a*static_pow<a,b-1>::value;
+  };
 
-    template <uint a>
-    struct static_pow<a,0>{
-        static const uint value = 1;
-    };
+  template <uint a>
+  struct static_pow<a,0> {
+    static const uint value = 1;
+  };
 
-    template <uint f>
-    struct flag{
-        static const uint value = static_pow<2,f>::value;
-    };
+  template <uint f>
+  struct flag {
+    static const uint value = static_pow<2,f>::value;
+  };
+
+  template <int size, int alignment>
+  struct align {
+    static const uint value = size+(alignment-(size%alignment));
+  };
 }
 
 #endif
