19e3b81f3397dd6e180365ac59a9e73692cc1547
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-05-21 23:06:20 +0200

    Added tmpfile default ctor and rewind

--- streams/FileStream.h
+++ streams/FileStream.h
@@ -7,11 +7,11 @@
 namespace L {
   class FileStream : public Stream {
     public:
+      inline FileStream() : Stream(tmpfile()) {}
       inline FileStream(const char* filepath, const char* mode) : Stream(fopen(filepath,mode)) {}
       inline FileStream(const File& file, const char* mode) : Stream(fopen(file.path().c_str(),mode)) {}
-      inline ~FileStream() {
-        fclose(_fd);
-      }
+      inline ~FileStream() {fclose(_fd);}
+      inline void rewind() {::rewind(_fd);}
   };
 }
 
