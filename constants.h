8c7cff9d9b8b294c2a1a96dc5e37413ed506a907
Lyatus (Box) <lutopia@hotmail.fr>
2015-05-27 12:18:55 +0200

    Added degrees to radians constant

--- constants.h
+++ constants.h
@@ -6,6 +6,10 @@ namespace L {
     static const T wtr((T)3.14159265358979323846);
     return wtr;
   }
+  template <class T> inline const T& DEGTORAD(){
+    static const T wtr(PI<T>()/(T)180);
+    return wtr;
+  }
 }
 
 #endif
