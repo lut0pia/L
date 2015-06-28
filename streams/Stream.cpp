eafd67707e261f1be460f2b21087f42c60f80456
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-06-28 17:34:07 +0200

    Rewrote Stream::line
    Now takes care of CRLF line ending too

--- streams/Stream.cpp
+++ streams/Stream.cpp
@@ -3,11 +3,11 @@
 using namespace L;
 
 const char* Stream::line() {
-  static char buffer[2048];
+  static char buffer[4096];
   char* w(buffer);
   nospace();
-  while((*w++ = get())!='\n') {}
-  unget('\n');
-  *--w = '\0';
+  while((*w++ = get())!='\n' && !eof()) {} // End of line or file
+  while(*(w-1)=='\n' || *(w-1)=='\r') w--; // Don't take \n into account
+  *w = '\0'; // Null-end string
   return buffer;
 }
