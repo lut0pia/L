59ef3d35f0386604c5c33ddcb2baca8847da1bc6
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-06-28 17:32:59 +0200

    Added Stream << char*

--- streams/Stream.h
+++ streams/Stream.h
@@ -28,6 +28,7 @@ namespace L {
       template<class T> inline Stream& operator>(T& v) {read(&v,sizeof(v)); return *this;}
 
       inline Stream& operator<<(char v) {fputc(v,_fd); return *this;}
+      inline Stream& operator<<(char* v) {fputs(v,_fd); return *this;}
       inline Stream& operator<<(const char* v) {fputs(v,_fd); return *this;}
       inline Stream& operator<<(int v) {fprintf(_fd,"%i",v); return *this;}
       inline Stream& operator<<(unsigned int v) {fprintf(_fd,"%u",v); return *this;}
