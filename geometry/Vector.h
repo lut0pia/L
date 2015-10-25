a3854e02a9b87056f55a17ade8c802984704244b
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-10-25 19:36:52 +0100

    Formatting

--- geometry/Vector.h
+++ geometry/Vector.h
@@ -141,8 +141,8 @@ namespace L {
       }
       Vector cross(const Vector& other) {
         return Vector(y()*other.z() - z()*other.y(),
-                     z()*other.x() - x()*other.z(),
-                     x()*other.y() - y()*other.x());
+                      z()*other.x() - x()*other.z(),
+                      x()*other.y() - y()*other.x());
       }
       T dot(const Vector& other) const {
         T wtr(0);
