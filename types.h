4d00f43d7137ae5990df0adb579ff04ae8dccf14
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-11-08 14:46:40 +0100

    Added uint32 type

--- types.h
+++ types.h
@@ -1,13 +1,17 @@
 #ifndef DEF_L_types
 #define DEF_L_types
 
+#include <cstdint>
+
 namespace L {
-	typedef unsigned char byte;
-	typedef unsigned short ushort;
-	typedef unsigned int uint;
-	typedef unsigned long ulong;
-	typedef long long llong;
-	typedef unsigned long long ullong;
+  typedef unsigned char byte;
+  typedef unsigned short ushort;
+  typedef unsigned int uint;
+  typedef unsigned long ulong;
+  typedef long long llong;
+  typedef unsigned long long ullong;
+
+  typedef uint32_t uint32;
 }
 
 #endif
