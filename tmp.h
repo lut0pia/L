abda51fee8edd6dfabf190cae50bcf9f46bebc77
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-11-15 16:57:46 +0100

    Simplified TMP definitions

--- tmp.h
+++ tmp.h
@@ -4,41 +4,11 @@
 #include "types.h"
 
 namespace L {
-  // ENABLE IF
-  template <bool B, class T = void>
-  struct enable_if_c {
-    typedef T type;
-  };
-
-  template <class T>
-  struct enable_if_c<false, T> {};
-
-  template <class Cond, class T = void>
-  struct enable_if : public enable_if_c<Cond::value, T> {};
-
-  // DISABLE IF
-  template <class Cond, class T = void>
-  struct disable_if : public enable_if_c<!Cond::value, T> {};
-
-
-  template <uint a, uint b>
-  struct static_pow {
-    static const uint value = a*static_pow<a,b-1>::value;
-  };
-
-  template <uint a>
-  struct static_pow<a,0> {
-    static const uint value = 1;
-  };
-
-  template <uint f>
-  struct flag {
-    static const uint value = static_pow<2,f>::value;
-  };
-  template <uint n>
-  struct bitmask {
-    static const uint value = ~(~0<<n);
-  };
+  template <class T, T v> struct constant {static const T value = v;};
+  template <int a, int b> struct static_pow : constant<int,a*static_pow<a,b-1>::value> {};
+  template <int a> struct static_pow<a,0> : constant<int,1> {};
+  template <uint f> struct flag : constant<uint,static_pow<2,f>::value> {};
+  template <uint n> struct bitmask : constant<uint,~(~0<<n)> {};
 
   template <class T>
   struct alignment {
