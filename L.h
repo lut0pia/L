138029586af2b627903f1a5b6c285dbbf7df52c4
Lyatus (LuduZord) <lutopia@hotmail.fr>
2015-12-08 23:08:03 +0100

    Rewrote dynamic sublib comment

--- L.h
+++ L.h
@@ -20,7 +20,7 @@
 #include "audio.h"          // Audio I/O, Wave & Midi
 #include "bytes.h"          // Byte to byte functions, encoding
 #include "containers.h"     // Classes that hold other classes
-#include "dynamic.h"        // Dynamic typing/casting
+#include "dynamic.h"        // Dynamic typing/casting/compiling
 #include "engine.h"         // Entity and components
 #include "font.h"           // Font handling
 #include "geometry.h"       // Vectors, intervals, surfaces, volumes, and all related functions
