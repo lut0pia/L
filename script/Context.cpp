8c050c87e57db4bfb202d7605d63a3e0048d5abb
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-01-07 22:16:34 +0100

    Script::Context now has basic features

--- script/Context.cpp
+++ script/Context.cpp
@@ -3,13 +3,56 @@
 using namespace L;
 using namespace Script;
 
+Var dofunc(Context& c, const Array<Var>& a) {
+  for(int i(1); i<a.size()-1; i++)
+    c.execute(a[i]);
+  return c.execute(a.back());
+}
+Var set(Context& c, const Array<Var>& a) {
+  Var target(c.execute(a[1]));
+  if(a.size()==3 && target.is<Symbol>())
+    return c.variable(target.as<Symbol>()) = c.execute(a[2]);
+  return 0;
+}
+Var iffunc(Context& c, const Array<Var>& a) {
+  if(a.size()>2) {
+    Var condition(c.execute(a[1]));
+    if(condition.get<bool>())
+      return c.execute(a[2]);
+    else if(a.size()>3)
+      return c.execute(a[3]);
+  }
+  return 0;
+}
+Var add(Context& c, int params) {
+  int wtr(0);
+  for(int i(0); i<params; i++)
+    if(c.parameter(i).is<int>())
+      wtr += c.parameter(i).as<int>();
+  return wtr;
+}
+Var print(Context& c, int params) {
+  for(int i(0); i<params; i++)
+    out << c.parameter(i);
+  return 0;
+}
+
+Context::Context() {
+  _frames.push(0); // Start current frame
+  _frames.push(0); // Start next frame (no variable)
+  variable(symbol("do")) = (Native)dofunc;
+  variable(symbol("set")) = (Native)set;
+  variable(symbol("if")) = (Native)iffunc;
+  variable(symbol("+")) = (Function)add;
+  variable(symbol("print")) = (Function)print;
+}
 void Context::read(Stream& stream) {
   Script::Lexer lexer(stream);
   Var v;
   lexer.nextToken();
   while(!stream.end()) {
     read(v,lexer);
-    execute(v,NULL);
+    execute(v);
   }
 }
 void Context::read(Var& v, Lexer& lexer) {
@@ -18,28 +61,73 @@ void Context::read(Var& v, Lexer& lexer) {
     int i(0);
     while(!lexer.acceptToken(")"))
       read(v[i++],lexer);
+  } else if(lexer.acceptToken("'")) {
+    v = Quote();
+    read(v.as<Quote>().var,lexer);
   } else {
-    String token(lexer.token());
-    if(function(token))
-      v = *function(token);
-    else v = token;
+    const char* token(lexer.token());
+    if(lexer.literal()) v = token; // Character string
+    else if(lexer.isToken("true")) v = true;
+    else if(lexer.isToken("false")) v = false;
+    else if(token[strspn(token,"0123456789")]=='\0') v = atoi(token); // Integer
+    else if(token[strspn(token,"0123456789.")]=='\0') v = (float)atof(token); // Float
+    else v = symbol(token);
     lexer.nextToken();
   }
 }
-void Context::declare(const String& name, const Var& code) {
-  *function(name) = code;
+
+Symbol Context::symbol(const char* str) {
+  // Search the known symbols
+  for(int i(0); i<_symbols.size(); i++)
+    if(!strcmp(_symbols[i].str(),str))
+      return _symbols[i];
+  // Create new symbol
+  char* sym((char*)malloc(strlen(str)));
+  strcpy(sym,str);
+  _symbols.push(sym);
+  return {sym};
 }
-Var* Context::function(const String& name) {
-  if(_functions[name].null())
-    _functions[name] = new Var();
-  return _functions[name];
+Var& Context::variable(Symbol sym) {
+  // Search the known variable
+  for(int i(nextFrame()-1); i>=0; i--)
+    if(_stack[i].key()==sym)
+      return _stack[i].value();
+  pushVariable(sym);
+  return _stack.back().value();
 }
-Var Context::execute(const Var& code, Var* stack) {
-  //out << code << '\n';
+void Context::pushVariable(Symbol sym, const Var& v) {
+  _stack.push(keyValue(sym,v));
+  _frames.back()++; // Added variable to current frame, must push next frame
+}
+Var Context::execute(const Var& code) {
   if(code.is<Array<Var> >()) { // Function call
-    const Var& handle(execute(code.as<Array<Var> >()[0],stack));
+    const Array<Var>& array(code.as<Array<Var> >()); // Get reference of array value
+    const Var& handle(execute(array[0])); // Execute first child of array to get function handle
     if(handle.is<Native>())
-      handle.as<Native>()(stack);
-  }
+      return handle.as<Native>()(*this,array);
+    else if(handle.is<Function>() || handle.is<Array<Var> >()) {
+      Var wtr;
+      _frames.push(nextFrame()); // Add new frame
+      for(int i(1); i<array.size(); i++) { // For all parameters
+        Symbol sym(symbol(""));
+        if(handle.is<Array<Var> >()
+            && handle.as<Array<Var> >()[0].as<Array<Var> >().size()>=i) {
+          Var symVar(execute(handle.as<Array<Var> >()[0].as<Array<Var> >()[i-1]));
+          if(symVar.is<Symbol>())
+            sym = symVar.as<Symbol>();
+        }
+        pushVariable(sym,execute(array[i])); // Compute parameter values
+      }
+      //out << _stack << '\n';
+      if(handle.is<Array<Var> >())
+        wtr = execute(handle.as<Array<Var> >()[1]);
+      else if(handle.is<Function>()) // It's a function pointer
+        wtr = handle.as<Function>()(*this,array.size()-1); // Call function
+      _frames.pop(); // Remove current frame
+      _stack.size(nextFrame()); // Remove elements from previous frame
+      return wtr;
+    }
+  } else if(code.is<Symbol>()) return variable(code.as<Symbol>()); // Evaluate symbol
+  else if(code.is<Quote>()) return code.as<Quote>().var; // Return raw data
   return code;
 }
--- script/Context.h
+++ script/Context.h
@@ -1,28 +1,47 @@
 #ifndef DEF_L_Script_Context
 #define DEF_L_Script_Context
 
-#include "../containers/StaticStack.h"
 #include "Lexer.h"
-#include "../containers/Map.h"
-#include "../containers/Ref.h"
 #include "../dynamic/Variable.h"
 
 namespace L {
   namespace Script {
-    typedef Var(*Native)(Var*);
+    class Context;
+    typedef Var(*Function)(Context&,int);
+    typedef Var(*Native)(Context&,const Array<Var>&);
+    typedef struct {Var var;} Quote;
+    class Symbol {
+      private:
+        const char* _str;
+      public:
+        inline Symbol() : _str(0) {}
+        inline Symbol(const char* str) : _str(str) {}
+        inline bool operator==(const Symbol& other) const {return _str==other._str;}
+        inline const char* str() const {return _str;}
+    };
     class Context {
       private:
-        StaticStack<4096,Var> _stack;
-        Map<String,Ref<Var> > _functions;
+        Array<KeyValue<Symbol,Var> > _stack;
+        Array<int> _frames;
+        Array<Symbol> _symbols;
 
       public:
+        Context();
         void read(Stream&);
         void read(Var& v, Lexer& lexer);
-        void declare(const String&, const Var&);
-        Var* function(const String&);
-        Var execute(const Var& code, Var* stack);
+
+        Symbol symbol(const char*);
+        Var& variable(Symbol);
+        void pushVariable(Symbol, const Var& = Var());
+        inline void pushParameter(const Var& v) {pushVariable(symbol(""),v);}
+        inline Var& Context::parameter(int i) {return _stack[currentFrame()+i].value();}
+        inline int currentFrame() const {return _frames[_frames.size()-2];}
+        inline int nextFrame() const {return _frames.back();}
+        Var execute(const Var& code);
     };
   }
+  inline Stream& operator<<(Stream& s, const Script::Symbol& v) {return s << v.str();}
+  inline Stream& operator<<(Stream& s, const Script::Quote& v) {return s << '\'' << v.var;}
 }
 
 #endif
