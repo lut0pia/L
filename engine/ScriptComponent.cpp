12c3780aafa45f41715353a92e600affe5b5691d
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-04-07 22:52:44 +0200

    Formatting

--- engine/ScriptComponent.cpp
+++ engine/ScriptComponent.cpp
@@ -5,24 +5,24 @@
 using namespace L;
 using namespace Script;
 
-ScriptComponent::ScriptComponent(){
-  _context.variable(_context.symbol("component")) = (Function)[](Context& c,int params)->Var{
-    if(params==2 && c.parameter(0).is<Entity*>() && c.parameter(1).is<String>()){
+ScriptComponent::ScriptComponent() {
+  _context.variable(_context.symbol("component")) = (Function)[](Context& c,int params)->Var {
+    if(params==2 && c.parameter(0).is<Entity*>() && c.parameter(1).is<String>()) {
       return c.parameter(0).as<Entity*>()->component(c.parameter(1).as<String>());
     } else return 0;
   };
-  _context.variable(_context.symbol("move")) = (Function)[](Context& c,int params)->Var{
+  _context.variable(_context.symbol("move")) = (Function)[](Context& c,int params)->Var {
     if(params==4 && c.parameter(0).is<Component*>())
       c.parameter(0).as<Transform*>()->move(Vector3f(c.parameter(1).get<float>(),c.parameter(2).get<float>(),c.parameter(3).get<float>()));
     return 0;
   };
 }
-void ScriptComponent::load(const char* filename){
-  _context.read(FileStream(filename,"rb"));
+void ScriptComponent::load(const char* filename) {
   _context.variable(_context.symbol("entity")) = entity();
+  _context.read(FileStream(filename,"rb"));
   _context.execute(_context.variable(_context.symbol("start")));
 }
-void ScriptComponent::update(){
+void ScriptComponent::update() {
   _context.variable(_context.symbol("delta")) = Engine::deltaSeconds();
   _context.execute(_context.variable(_context.symbol("update")));
 }
