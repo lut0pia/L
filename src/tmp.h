650bf95cead26d6ac1f7c0c36840d5c76c80bdd6
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-04-18 23:44:07 +0200

    Using #pragma once in tmp

--- src/tmp.h
+++ src/tmp.h
@@ -1,5 +1,4 @@
-#ifndef DEF_L_tmp
-#define DEF_L_tmp
+#pragma once
 
 #include "types.h"
 
@@ -10,7 +9,3 @@ namespace L {
   template <uint32_t f> struct flag : constant<uint32_t,static_pow<2,f>::value> {};
   template <uint32_t n> struct bitmask : constant<uint32_t,~(~0<<n)> {};
 }
-
-#endif
-
-
