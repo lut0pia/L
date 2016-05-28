b5f9d7bacfa62b9b94835605bbe2a1115b66600e
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-05-29 00:38:18 +0200

    Added overloadable hash function

--- src/hash.h
+++ src/hash.h
@@ -12,10 +12,19 @@ namespace L{
     }
     return wtr;
   }
-  constexpr uint32_t FNV1A(const char* str,uint32_t r){
-    return (*str) ? FNV1A(str+1,(r^*str)*16777619) : r;
+  inline uint32_t fnv1a(const char* data,size_t size){
+    uint32_t wtr(2166136261);
+    while(size--) {
+      wtr ^= *data;
+      wtr *= 16777619;
+      data++;
+    }
+    return wtr;
   }
-  constexpr uint32_t FNV1A(const char* str){
-    return FNV1A(str,2166136261);
-  }
+  constexpr uint32_t FNV1A(const char* str,uint32_t r){ return (*str) ? FNV1A(str+1,(r^*str)*16777619) : r; }
+  constexpr uint32_t FNV1A(const char* str){ return FNV1A(str,2166136261); }
+
+  template <class T> inline uint32_t hash(const T& v){ return fnv1a(&v,sizeof(v)); }
+  inline uint32_t hash(const char* str){ return fnv1a(str); }
+  inline uint32_t hash(uint32_t v){ return v; }
 }
