dbce360fe43ad493692d466fa2b0527ef83e8805
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-05-30 23:55:44 +0200

    Faster Pool

--- src/containers/Pool.h
+++ src/containers/Pool.h
@@ -6,34 +6,40 @@
 namespace L {
   template <class T>
   class Pool {
-    static const int tableSize = 16;
-    static const int intBits = sizeof(int)*8;
+    static const uint32_t tableSize = 16;
+    static const uint32_t intBits = sizeof(uint32_t)*8;
   private:
     class Block {
     public:
-      static const int size = tableSize*intBits;
+      static const uint32_t size = tableSize*intBits;
       byte _data[size*sizeof(T)];
       uint32_t _table[tableSize]; // Every bit of this array represents a object slot in _data
       Block* _next; // Pool works like a linked list, in case no more space is available
-      bool _full;
-      inline Block() : _table{0},_next(nullptr),_full(false){}
+      uint32_t _head,_tail;
+      inline Block() : _table{0},_next(nullptr),_head(0),_tail(0){}
       inline ~Block() { delete _next; }
-      inline bool allocated(int i) const{ return (_table[i/intBits]&(1<<(i%intBits)))!=0; } // Check if element i is allocated
-      inline T* pointer(int i) const{ return ((T*)_data) + i; } // Returns pointer to ith element
-      inline T* allocate(int i) {
+      inline bool allocated(uint32_t i) const{ return i<_head || (_table[i/intBits]&(1u<<(i%intBits)))!=0; } // Check if element i is allocated
+      inline bool full() const{ return _head==size; }
+      inline T* pointer(uint32_t i) const{ return ((T*)_data) + i; } // Returns pointer to ith element
+      inline T* allocate() {
+        uint32_t i(_head);
+        while(allocated(++_head)); // Move head to first unallocated slot
+        if(i>_tail) _tail = i;
         _table[i/intBits] |= 1<<(i%intBits); // Mark slot allocated
         return pointer(i); // Return pointer to element
       }
-      inline void deallocate(int i) {
-        _table[i/intBits] &= ~(1<<(i%intBits)); // Mark slot as free
-        _full = false; // The block is not full
+      inline void deallocate(uint32_t i) {
+        if(i<_head) _head = i;
+        if(_tail==i) _tail--;
+        _table[i/intBits] &= ~(1u<<(i%intBits)); // Mark slot as free
       }
+      inline void deallocate(void* p){ deallocate(((uintptr_t)p-(uintptr_t)_data)/sizeof(T)); }
       inline bool hasAddress(void *p) const{ return _data<=p && p<_data+sizeof(_data); }
     };
     class Iterator{
     private:
       Block* _block;
-      int _i;
+      uint32_t _i;
 
     public:
       Iterator() : _block(0),_i(0){}
@@ -42,12 +48,11 @@ namespace L {
       Iterator& operator++(){
         if(_block){
           do{
-            if(++_i==Block::size){ // End of the block
+            if(++_i>_block->_tail){ // End of the block
               _block = _block->_next;
               _i = 0;
-              if(!_block) return *this; // No more blocks
             }
-          } while(!_block->allocated(_i)); // Stop when allocated slot is found
+          } while(_block && !_block->allocated(_i)); // Stop when no more block or allocated slot is found
         }
         return *this;
       }
@@ -80,26 +85,14 @@ namespace L {
     }
     T* allocate() {
       if(!_root) _root = new Block();
-      Block* block(_root); // Start with the root block
-      while(true){ // Repeat until allocated
-        if(!block->_full){
-          for(int i(0); i<Block::size; i++)
-            if(!block->allocated(i)) // This slot is not allocated
-              return block->allocate(i); // Allocate that slot
-          block->_full = true; // If here then the block is full
-        }
+      for(Block* block(_root);; block = block->_next){
+        if(!block->full()) return block->allocate();
         if(!block->_next) block->_next = new Block(); // Make sure there's a next block
-        block = block->_next; // Continue with next block
       }
     }
     void deallocate(void* p) {
-      Block* block(_root);
-      while(block){ // Stop when the block doesn't exist
-        if(block->hasAddress(p)) { // Pointer is in this block
-          int i(((intptr_t)p-(intptr_t)block->_data)/sizeof(T)); // Compute element's index
-          block->deallocate(i);
-        } else block = block->_next;
-      }
+      for(Block* block(_root); block; block = block->_next) // Stop when the block doesn't exist
+        if(block->hasAddress(p)) return block->deallocate(p);
     }
     inline Iterator begin() const{ return Iterator(_root); }
     inline Iterator end() const{ return Iterator(nullptr); }
