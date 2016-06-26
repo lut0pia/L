b2c4ffa4ecc910fce4ee98c3caa0df58ace29cb7
Lyatus (LuduZord) <lutopia@hotmail.fr>
2016-06-26 10:03:28 +0200

    Added Transform::rotateAbsolute and format

--- src/engine/Transform.h
+++ src/engine/Transform.h
@@ -7,31 +7,35 @@
 
 namespace L {
   class Transform : public Component {
-      L_COMPONENT(Transform)
-    private:
-      Transform* _parent;
-      Vector3f _translation;
-      Quatf _rotation;
-    public:
-      inline Transform() : _parent(nullptr), _translation(0,0,0) {}
-      inline Vector3f absolutePosition() const {return (_parent)?(_parent->absolutePosition()+parentRotation().rotate(_translation)):_translation;}
-      inline Vector3f toAbsolute(const Vector3f& v) const {return absolutePosition()+_rotation.rotate(v);}
-      inline Quatf absoluteRotation() const {return (_parent)?_parent->absoluteRotation():_rotation;}
-      inline Quatf parentRotation() const {return (_parent)?_parent->absoluteRotation():Quatf();}
-      inline void rotate(const Quatf& q) {_rotation = _rotation * q;}
-      inline void rotate(const Vector3f& v, float d) {rotate(Quatf(v,d));}
-      inline void rotateAbsolute(const Quatf& q){ _rotation = q * _rotation; } // Wrong
-      inline void rotateAbsolute(const Vector3f& v,float d) { rotateAbsolute(Quatf(v,d)); }
-      inline void phiLook(float d) {rotate(Vector3f(1,0,0),d);}
-      inline void thetaLook(float d) {rotate(Vector3f(0,0,1),d);}
-      inline void move(const Vector3f& d) {_translation += _rotation.rotate(d);}
-      inline void moveAbsolute(const Vector3f& v) {_translation += parentRotation().inverse().rotate(v);}
+    L_COMPONENT(Transform)
+  private:
+    Transform* _parent;
+    Vector3f _translation;
+    Quatf _rotation;
+  public:
+    inline Transform() : _parent(nullptr),_translation(0,0,0) {}
+    inline Vector3f absolutePosition() const { return (_parent) ? (_parent->absolutePosition()+parentRotation().rotate(_translation)) : _translation; }
+    inline Vector3f toAbsolute(const Vector3f& v) const { return absolutePosition()+_rotation.rotate(v); }
+    inline Quatf absoluteRotation() const { return (_parent) ? _parent->absoluteRotation() : _rotation; }
+    inline Quatf parentRotation() const { return (_parent) ? _parent->absoluteRotation() : Quatf(); }
+    inline void rotate(const Quatf& q) { _rotation = _rotation * q; }
+    inline void rotate(const Vector3f& v,float d) { rotate(Quatf(v,d)); }
+    inline void rotateAbsolute(const Quatf& q){ _rotation = q * _rotation; } // Wrong
+    inline void rotateAbsolute(const Vector3f& v,float d) { rotateAbsolute(Quatf(v,d)); }
+    inline void phiLook(float d) { rotate(Vector3f(1,0,0),d); }
+    inline void thetaLook(float d) { rotate(Vector3f(0,0,1),d); }
+    inline void move(const Vector3f& d) { _translation += _rotation.rotate(d); }
+    inline void moveAbsolute(const Vector3f& v) { _translation += parentRotation().inverse().rotate(v); }
 
-      inline void parent(Transform* p) {_parent = p;}
-      inline Transform* parent() const {return _parent;}
-      inline Vector3f right() const {return absoluteRotation().rotate(Vector3f(1,0,0));}
-      inline Vector3f forward() const {return absoluteRotation().rotate(Vector3f(0,1,0));}
-      inline Vector3f up() const {return absoluteRotation().rotate(Vector3f(0,0,1));}
-      inline Matrix44f matrix() const {return Matrix44f::translation(absolutePosition())*Matrix44f::orientation(right(),forward(),up());}
+    inline void parent(Transform* p) { _parent = p; }
+    inline Transform* parent() const { return _parent; }
+    inline void translation(const Vector3f& t){ _translation = t; }
+    inline const Vector3f& translation() const{ return _translation; }
+    inline void rotation(const Quatf& r){ _rotation = r; }
+    inline const Quatf& rotation() const{ return _rotation; }
+    inline Vector3f right() const { return absoluteRotation().rotate(Vector3f(1,0,0)); }
+    inline Vector3f forward() const { return absoluteRotation().rotate(Vector3f(0,1,0)); }
+    inline Vector3f up() const { return absoluteRotation().rotate(Vector3f(0,0,1)); }
+    inline Matrix44f matrix() const { return Matrix44f::translation(absolutePosition())*Matrix44f::orientation(right(),forward(),up()); }
   };
 }
