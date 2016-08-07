b64ae5747c9fe96126b11afc3543036b826b1808
Lucien Catonnet <lutopia@hotmail.fr>
2016-08-07 11:52:17 +0200

    Added sphere collisions

--- src/engine/Collider.cpp
+++ src/engine/Collider.cpp
@@ -28,7 +28,7 @@ Interval3f Collider::boundingBox() const {
       return Interval3f(_center-_radius,_center+_radius).transformed(_transform->matrix());
     case Sphere:
     {
-      Vector3f center(_transform->toAbsolute(_center));
+      const Vector3f center(_transform->toAbsolute(_center));
       return Interval3f(center-_radius,center+_radius);
     }
     default:
@@ -66,7 +66,23 @@ Vector3f leastToAxis(const Vector3f& axis,const Vector3f* points,size_t count) {
   return Vector3f();
 }
 void Collider::checkCollision(Collider& a,Collider& b) {
-  if(a._type==Box && b._type==Box) {
+  Vector3f impactPoint,normal;
+  if(a._type==Sphere && b._type==Sphere){
+    const Vector3f apos(a._transform->toAbsolute(a._center)),
+      bpos(b._transform->toAbsolute(b._center)),
+      btoa(apos-bpos);
+    const float distance(btoa.length()),
+      overlap((a._radius.x()+b._radius.x())-distance);
+    if(overlap>.0f){
+      normal = btoa.normalized();
+      impactPoint = bpos+normal*b._radius.x();
+      // Resolve interpenetration
+      if(b._rigidbody){
+        a._transform->moveAbsolute(normal*overlap*.5f);
+        b._transform->moveAbsolute(normal*overlap*-.5f);
+      } else a._transform->moveAbsolute(normal*overlap);
+    } else return;
+  } else if(a._type==Box && b._type==Box) {
     const Transform *at(a._transform),*bt(b._transform);
     const Vector3f ar(at->right()),af(at->forward()),au(at->up()),
       br(bt->right()),bf(bt->forward()),bu(bt->up());
@@ -104,7 +120,6 @@ void Collider::checkCollision(Collider& a,Collider& b) {
     };
     uintptr_t axis(sizeof(axes));
     float minOverlap;
-    Vector3f normal;
     for(uintptr_t i(0); i<sizeof(axes)/sizeof(Vector3f); i++) {
       if(axes[i].lengthSquared()>0.00001f) { // The axis is not a null vector (caused by a cross product)
         Interval1f axisA(project(axes[i],apoints,8)),axisB(project(axes[i],bpoints,8)),intersection(axisA,axisB); // Compute projections and intersection
@@ -124,7 +139,6 @@ void Collider::checkCollision(Collider& a,Collider& b) {
       b._transform->moveAbsolute(normal*minOverlap*-.5f);
     } else a._transform->moveAbsolute(normal*minOverlap);
     // Compute impact point
-    Vector3f impactPoint(0,0,0);
     if(axis<6)
       impactPoint = (axis<3) ? leastToAxis(-normal,bpoints,8) : leastToAxis(normal,apoints,8);
     else{
@@ -134,16 +148,41 @@ void Collider::checkCollision(Collider& a,Collider& b) {
         return; // Unable to compute intersection
       impactPoint = (avertex+bvertex)/2.f;
     }
-    auto e(ref<Table<Var,Var>>());
-    (*e)[FNV1A("type")] = FNV1A("COLLISION");
-    if(a._script){
-      (*e)[FNV1A("other")] = &b;
-      a._script->event(e);
+  } else{ // Box-Sphere
+    Collider *box,*sphere;
+    if(a._type==Box){
+      box = &a;
+      sphere = &b;
+    } else{
+      box = &b;
+      sphere = &a;
     }
-    if(b._script){
-      (*e)[FNV1A("other")] = &a;
-      b._script->event(e);
-    }
-    RigidBody::collision(a._rigidbody,b._rigidbody,impactPoint,normal);
+    const Vector3f sphereCenter(sphere->_transform->toAbsolute(sphere->_center));
+    const Vector3f relCenter(box->_transform->fromAbsolute(sphereCenter));
+    const Vector3f closest(clamp(relCenter,-box->_radius,box->_radius));
+    impactPoint = box->_transform->toAbsolute(closest);
+    const float overlap(sphere->_radius.x()-impactPoint.dist(sphereCenter));
+    if(overlap>.0f){
+      normal = (box==&a) ? impactPoint-sphereCenter : sphereCenter-impactPoint;
+      normal.normalize();
+      // Resolve interpenetration
+      if(b._rigidbody){
+        a._transform->moveAbsolute(normal*overlap*.5f);
+        b._transform->moveAbsolute(normal*overlap*-.5f);
+      } else a._transform->moveAbsolute(normal*overlap);
+    } else return; // No collision
   }
+  // Send collision events to scripts
+  auto e(ref<Table<Var,Var>>());
+  (*e)[FNV1A("type")] = FNV1A("COLLISION");
+  if(a._script){
+    (*e)[FNV1A("other")] = &b;
+    a._script->event(e);
+  }
+  if(b._script){
+    (*e)[FNV1A("other")] = &a;
+    b._script->event(e);
+  }
+  // Physically resolve collision
+  RigidBody::collision(a._rigidbody,b._rigidbody,impactPoint,normal);
 }
