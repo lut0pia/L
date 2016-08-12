8d6d38723649c0cc4ba56526be29bf693cf9b320
Lucien Catonnet <lutopia@hotmail.fr>
2016-08-12 19:12:23 +0200

    Using vec4s for dither matrix

--- src/engine/SharedUniform.h
+++ src/engine/SharedUniform.h
@@ -1,21 +1,21 @@
 #pragma once 
 
-#define L_SHAREDUNIFORM_SIZE (sizeof(Matrix44f)*64+sizeof(Matrix44f)*2+sizeof(Vector4i)+sizeof(Vector3f))
-#define L_SHAREDUNIFORM_DITHERMATRIX 0
-#define L_SHAREDUNIFORM_VIEWPROJ (L_SHAREDUNIFORM_DITHERMATRIX+sizeof(Matrix44f)*64)
+#define L_SHAREDUNIFORM_SIZE (sizeof(Matrix44f)*2+sizeof(Vector4f)*256+sizeof(Vector4i)+sizeof(Vector3f))
+#define L_SHAREDUNIFORM_VIEWPROJ 0
 #define L_SHAREDUNIFORM_INVVIEWPROJ (L_SHAREDUNIFORM_VIEWPROJ+sizeof(Matrix44f))
-#define L_SHAREDUNIFORM_DITHERMATRIXSIZE (L_SHAREDUNIFORM_INVVIEWPROJ+sizeof(Matrix44f))
+#define L_SHAREDUNIFORM_DITHERMATRIX (L_SHAREDUNIFORM_INVVIEWPROJ+sizeof(Matrix44f))
+#define L_SHAREDUNIFORM_DITHERMATRIXSIZE (L_SHAREDUNIFORM_DITHERMATRIX+sizeof(Vector4f)*256)
 #define L_SHAREDUNIFORM_EYE (L_SHAREDUNIFORM_DITHERMATRIXSIZE+sizeof(Vector4i))
 #define L_SHAREDUNIFORM \
 "layout (std140) uniform Shared {" \
-"mat4 ditherMatrix[64];" \
 "mat4 viewProj, invViewProj;" \
+"vec4 ditherMatrix[256];" \
 "ivec4 ditherMatrixSize;" \
 "vec3 eye;" \
 "};" \
 "bool alpha(float a){" \
 "ivec2 ditherPos = ivec2(mod(gl_FragCoord.xy,ditherMatrixSize.xy));" \
 "int ditherSlot = ditherPos.x+ditherPos.y*ditherMatrixSize.x;" \
-"float ditherThreshold = ditherMatrix[ditherSlot/16][(ditherSlot%16)/4][ditherSlot%4];" \
+"float ditherThreshold = ditherMatrix[ditherSlot/4][ditherSlot%4];" \
 "return a<ditherThreshold;" \
 "}"
