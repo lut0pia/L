dfcf62490b580b35c86a58d7de81495cfe712eb8
Lucien Catonnet <lutopia@hotmail.fr>
2016-09-06 20:10:46 +0200

    Added Entity::components getter

--- src/engine/Entity.h
+++ src/engine/Entity.h
@@ -7,44 +7,45 @@
 namespace L {
   class Component;
   class Entity {
-    private:
-      Array<KeyValue<const TypeDescription*,Component*> > _components;
+  private:
+    Array<KeyValue<const TypeDescription*,Component*> > _components;
 
-    public:
-      inline Entity() {}
-      Entity(const Entity* other);
-      ~Entity();
-      inline void* operator new(size_t size) {return Pool<Entity>::global.allocate();}
-      inline void operator delete(void* p) {Pool<Entity>::global.deallocate((Entity*)p);}
+  public:
+    inline Entity() {}
+    Entity(const Entity* other);
+    ~Entity();
+    const auto& components() const{ return _components; }
+    inline void* operator new(size_t size) { return Pool<Entity>::global.allocate(); }
+    inline void operator delete(void* p) { Pool<Entity>::global.deallocate((Entity*)p); }
 
-      template <class CompType>
-      CompType* component() const {
-        for(auto&& pair : _components)
-          if(pair.key()==Type<CompType>::description())
-            return (CompType*)pair.value();
-        return 0;
-      }
-      Component* component(const char* name) const {
-        for(uint32_t i(0); i<_components.size(); i++)
-          if(!strcmp(_components[i].key()->name,name))
-            return _components[i].value();
-        return 0;
-      }
-      template <class CompType>
-      CompType* requireComponent() {
-        CompType* wtr(component<CompType>());
-        if(wtr) return wtr;
-        else return add<CompType>();
-      }
-      template <class CompType>
-      CompType* add() {
-        CompType* component(Pool<CompType>::global.construct());
-        component->entity(this);
-        _components.push(keyValue(Type<CompType>::description(),(Component*)component)); // Link the entity to the component
-        updateComponents();
-        return component;
-      }
-      void updateComponents();
-      void remove(Component*);
+    template <class CompType>
+    CompType* component() const {
+      for(auto&& pair : _components)
+        if(pair.key()==Type<CompType>::description())
+          return (CompType*)pair.value();
+      return 0;
+    }
+    Component* component(const char* name) const {
+      for(uint32_t i(0); i<_components.size(); i++)
+        if(!strcmp(_components[i].key()->name,name))
+          return _components[i].value();
+      return 0;
+    }
+    template <class CompType>
+    CompType* requireComponent() {
+      CompType* wtr(component<CompType>());
+      if(wtr) return wtr;
+      else return add<CompType>();
+    }
+    template <class CompType>
+    CompType* add() {
+      CompType* component(Pool<CompType>::global.construct());
+      component->entity(this);
+      _components.push(keyValue(Type<CompType>::description(),(Component*)component)); // Link the entity to the component
+      updateComponents();
+      return component;
+    }
+    void updateComponents();
+    void remove(Component*);
   };
 }
