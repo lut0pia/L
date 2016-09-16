c049c65b6e17f533ab8b1d36f0d3ff70d848d7d1
Lucien Catonnet <lutopia@hotmail.fr>
2016-09-16 18:46:32 +0200

    Removed RWSync

--- src/L.h
+++ src/L.h
@@ -124,7 +124,6 @@
 #include "parallelism/Coroutine.h"
 #include "parallelism/Fiber.h"
 #include "parallelism/Mutex.h"
-#include "parallelism/RWSync.h"
 #include "parallelism/Semaphore.h"
 #include "parallelism/Thread.h"
 
