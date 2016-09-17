992d9dee18bb92a7b200d7fdb060fe095b24532a
Lucien Catonnet <lutopia@hotmail.fr>
2016-09-17 17:37:44 +0200

    Superficial Thread update

--- src/parallelism/Thread.cpp
+++ src/parallelism/Thread.cpp
@@ -3,45 +3,44 @@
 using namespace L;
 
 void Thread::proxy(Thread* thread) {
-  thread->running = true;
-  thread->result = thread->function(thread);
-  thread->running = false;
-}
-Thread::Thread() : running(false), waitedFor(false) {}
-Thread::Thread(Function f, const Var& a) : running(true), waitedFor(false) {
-  start(f,a);
-}
-Thread::~Thread() {
-  terminate();
+  thread->_running = true;
+  thread->_result = thread->_function(thread);
+  thread->_running = false;
 }
 
-void Thread::start(Function f, const Var& a) {
-  function = f;
-  arg = a;
+void Thread::start(Function f,const Var& a) {
+  _function = f;
+  _arg = a;
 #if defined L_WINDOWS
-  threadHandle = CreateThread(nullptr,0,(LPTHREAD_START_ROUTINE)proxy,(LPVOID)this,0,&threadId);
+  _threadHandle = CreateThread(nullptr,0,(LPTHREAD_START_ROUTINE)proxy,(LPVOID)this,0,&_threadId);
 #elif defined L_UNIX
-  pthread_create(&thread,nullptr,(void* (*)(void*))proxy,this);
+  pthread_create(&_thread,nullptr,(void* (*)(void*))proxy,this);
 #endif
 }
-void Thread::waitForEnd(int maxMilliseconds) {
-  if(running) {
-    waitedFor = true;
+void Thread::wait(int maxMilliseconds) {
+  if(_running) {
+    _waitedFor = true;
 #if defined L_WINDOWS
-    WaitForSingleObject(threadHandle,maxMilliseconds);
+    WaitForSingleObject(_threadHandle,maxMilliseconds);
 #elif defined L_UNIX
-    pthread_join(thread,nullptr);
+    pthread_join(_thread,nullptr);
 #endif
   }
 }
 void Thread::terminate() {
-  if(running) {
+  if(_running) {
 #if defined L_WINDOWS
-    TerminateThread(threadHandle,0);
+    TerminateThread(_threadHandle,0);
 #elif defined L_UNIX
-    pthread_cancel(thread);
+    pthread_cancel(_thread);
 #endif
-    running = false;
+    _running = false;
   }
 }
-
+void Thread::affinity(uint32_t mask){
+#if defined L_WINDOWS
+  SetThreadAffinityMask(_threadHandle,mask);
+#elif defined L_UNIX
+#error Checkout pthread_setaffinity_np
+#endif
+}
--- src/parallelism/Thread.h
+++ src/parallelism/Thread.h
@@ -1,5 +1,4 @@
-#ifndef DEF_L_Thread
-#define DEF_L_Thread
+#pragma once
 
 #if defined L_WINDOWS
 #include <windows.h>
@@ -7,46 +6,41 @@
 #include <pthread.h>
 #endif
 
-#include <cstdio>
-#include <cstdlib>
 #include "../dynamic/Variable.h"
 #include "../macros.h"
 
 namespace L {
   class Thread {
-      typedef Var(*Function)(Thread*);
-    private:
-      Function function;
-      Var arg, result;
-      bool running, waitedFor;
+    L_NOCOPY(Thread)
+  public:
+    typedef Var(*Function)(Thread*);
+  private:
+    Function _function;
+    Var _arg,_result;
+    bool _running : 1,_waitedFor : 1;
 #if defined L_WINDOWS
-      DWORD threadId;
-      HANDLE threadHandle;
+    DWORD _threadId;
+    HANDLE _threadHandle;
 #elif defined L_UNIX
-      pthread_t thread;
+    pthread_t _thread;
 #endif
-      static void proxy(Thread*);
+    static void proxy(Thread*);
 
-    public:
-      Thread();
-      Thread(Function,const Var& = Var());
-      L_NOCOPY(Thread)
-      ~Thread();
+  public:
+    inline Thread() : _running(false),_waitedFor(false) {}
+    inline Thread(Function f,const Var& a = Var()) : _running(true),_waitedFor(false) { start(f,a); }
+    inline ~Thread() { terminate(); }
 
-      void start(Function,const Var& = Var());
-      void waitForEnd(int maxMilliseconds = -1);
-      void terminate();
+    void start(Function,const Var& = Var());
+    void wait(int maxMilliseconds = -1);
+    void terminate();
+    void affinity(uint32_t mask);
 
-      inline const Var& gArg() const {return arg;}
-      inline const Var& gResult() const {return result;}
-      inline bool isRunning() const {return running;}
-      inline bool isWaitedFor() const {return waitedFor;}
+    inline const Var& arg() const { return _arg; }
+    inline const Var& result() const { return _result; }
+    inline bool running() const { return _running; }
+    inline bool waitedFor() const { return _waitedFor; }
 
-      friend class ThreadPool;
+    friend class ThreadPool;
   };
 }
-#endif
-
-
-
-
