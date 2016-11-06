f19f9d596e37e3693d9a352c79d47c8122761a1d
Lucien Catonnet <lutopia@hotmail.fr>
2016-11-06 16:22:07 +0100

    Added compound assignment operators to script

--- src/script/Context.cpp
+++ src/script/Context.cpp
@@ -223,6 +223,20 @@ Context::Context(){
   CMP(">=",<);
   CMP("<=",>);
 #undef CMP
+#define SETOP(op)\
+  _globals[Symbol(#op)] = (Native)([](Context& c,const Array<Var>& a)->Var {\
+    L_ASSERT(a.size()>1);\
+    Var* target(c.reference(a[1]));\
+    for(int i(2);i<a.size();i++)\
+      *target op c.execute(a[i]);\
+    return 0;\
+  })
+  SETOP(+= );
+  SETOP(-= );
+  SETOP(*= );
+  SETOP(/= );
+  SETOP(%= );
+#undef SETOP
   _globals[Symbol("+")] = (Function)([](const Var&,SymbolVar* stack,size_t params)->Var {
     L_ASSERT(params>=1);
     Var wtr(stack[0].value());
