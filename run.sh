e56628702cc5b9d91fdb48e80165064612992b3e
Lucien Catonnet <lutopia@hotmail.fr>
2016-12-10 14:13:55 +0100

    Premake no longer a binary on Linux but cloned and built

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Premake
+(git clone https://github.com/premake/premake-core pmk) || # Attempt to clone
+(!(git status pmk | grep up-to-date) && cd pmk && git pull && cd ..) # Otherwise pull if not up-to-date
+
+if [ ! -e premake5 ] || [ premake5 -ot pmk ] ; then # If it hasn't been built or is older than repo
+	(cd pmk && make -f Bootstrap.mak linux && cd ..) && # Build premake
+	cp pmk/bin/release/premake5 premake5 # Copy binary
+fi
+
+# L
+configuration=${1:-development} #Configuration is development by default
+case $configuration in
+   "debug") exe="Ldbg"
+   ;;
+   "development") exe="Ldev"
+   ;;
+   "release") exe="L"
+   ;;
+esac
+./premake5 gmake && (cd prj/gmake && make -j 4 config=$configuration) && # Run premake and make
+(cd smp && ./$exe) # Execute program
