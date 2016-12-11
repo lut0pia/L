c1e83e67844fca5eaf25ec7222eb72ddf65abd7d
Lucien Catonnet <lutopia@hotmail.fr>
2016-12-11 11:47:21 +0100

    Interface uses Table

--- src/Interface.h
+++ src/Interface.h
@@ -1,76 +1,75 @@
 #pragma once
 
 #include "macros.h"
-#include "containers/Map.h"
+#include "containers/Table.h"
 #include "String.h"
-#include "containers/Array.h"
 #include "system/File.h"
 #include "streams/FileStream.h"
-#include "hash.h"
 
 namespace L {
   template <class T>
   class Interface {
-    private:
-      static Map<uint32_t,Interface*> instance;
+  private:
+    static Table<const char*,Interface*> instance;
 
-    protected:
-      Interface(const char* format) {
-        subscribe(format);
-      }
-      void subscribe(const char* format) {
-        instance[fnv1a(format)] = this;
-      }
+  protected:
+    Interface(const char* format) {
+      subscribe(format);
+    }
+    void subscribe(const char* format) {
+      instance[format] = this;
+    }
 
-    public:
-      virtual bool from(T& v, const File& file) {
-        FileStream fs(file.path(),"rb");
-        return from(v,fs);
-      }
-      virtual bool from(T& v, const char* str) {
-        tmpfile.rewind();
-        tmpfile << str;
-        tmpfile.rewind();
-        return from(v,tmpfile);
-      }
-      virtual bool from(T& v, Stream& is) {
-        return false;
-      }
-      virtual bool from(T& v, const Array<byte>& bytes) {
-        return false;
-      }
+  public:
+    virtual bool from(T& v,const File& file) {
+      FileStream fs(file.path(),"rb");
+      return from(v,fs);
+    }
+    virtual bool from(T& v,const char* str) {
+      tmpfile.rewind();
+      tmpfile << str;
+      tmpfile.rewind();
+      return from(v,tmpfile);
+    }
+    virtual bool from(T& v,Stream& is) {
+      return false;
+    }
+    virtual bool from(T& v,const Array<byte>& bytes) {
+      return false;
+    }
 
-      virtual bool to(const T& v, const File& file) {
-        FileStream stream(file.path(),"wb");
-        return to(v,stream);
-      }
-      virtual bool to(const T& v, String& str) {
+    virtual bool to(const T& v,const File& file) {
+      FileStream stream(file.path(),"wb");
+      return to(v,stream);
+    }
+    virtual bool to(const T& v,String& str) {
+      return false;
+    }
+    virtual bool to(const T& v,Stream& os) {
+      return false;
+    }
+    virtual bool to(const T& v,Array<byte>& bytes) {
+      tmpfile.rewind();
+      if(!to(v,tmpfile)) // Write in stream
         return false;
-      }
-      virtual bool to(const T& v, Stream& os) {
-        return false;
-      }
-      virtual bool to(const T& v, Array<byte>& bytes) {
-        tmpfile.rewind();
-        if(!to(v,tmpfile)) // Write in stream
-          return false;
-        bytes.size(tmpfile.tell());
-        tmpfile.rewind();
-        tmpfile.read(&bytes[0],bytes.size());
-        tmpfile.rewind();
-        return true;
-      }
+      bytes.size(tmpfile.tell());
+      tmpfile.rewind();
+      tmpfile.read(&bytes[0],bytes.size());
+      tmpfile.rewind();
+      return true;
+    }
 
-      static Interface& in(const char* format) {
-        if(instance.has(fnv1a(format))) return *instance[fnv1a(format)];
-        else L_ERRORF("Unhandled format %s",format);
-      }
-      static void fromFile(T& v, const String& path) {
-        in(path.explode('.').back().toLower()).from(v,File(path));
-      }
-      static void toFile(const T& v, const String& path) {
-        in(path.explode('.').back().toLower()).to(v,File(path));
-      }
+    static Interface& in(const char* format) {
+      const auto& slot(instance.find(format));
+      if(slot) return *slot->value();
+      else L_ERRORF("Unhandled format %s",format);
+    }
+    static void fromFile(T& v,const String& path) {
+      in(path.explode('.').back().toLower()).from(v,File(path));
+    }
+    static void toFile(const T& v,const String& path) {
+      in(path.explode('.').back().toLower()).to(v,File(path));
+    }
   };
-  template <class T> Map<uint32_t,Interface<T>*> Interface<T>::instance;
+  template <class T> Table<const char*,Interface<T>*> Interface<T>::instance;
 }
