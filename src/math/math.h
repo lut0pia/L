32409c52ba0085c5b74415e42c3f178f530d7dde
Lucien Catonnet <lutopia@hotmail.fr>
2016-12-26 16:52:41 +0100

    Added int32_t log2 and clog2

--- src/math/math.h
+++ src/math/math.h
@@ -2,39 +2,42 @@
 
 #include <cmath>
 #include "../types.h"
+#include "../system/intrinsics.h"
 
 namespace L {
-  template <int base = 2,class T>
-  const T& log(const T& v) {
-  }
-  template <class T> inline constexpr T log(const T& x,const T& base) { return log(x)/log(base); }
-  template <class T> inline constexpr const T& max(const T& a,const T& b) { return (a>b) ? a : b; }
-  template <class T> inline constexpr const T& min(const T& a,const T& b) { return (a<b) ? a : b; }
-  template <class T> inline constexpr const T& clamp(const T& v,const T& min,const T& max) { return (v<min) ? min : ((v>max) ? max : v); }
-  template <class T> inline constexpr T abs(const T& n) { return (n<0) ? -n : n; }
-  template <class T> inline constexpr T sqr(const T& x){ return x*x; }
-  inline constexpr uint32_t bitmask(uint32_t bits){ return ~(~0<<bits); }
+  // Logs
+  template <class T> T log2(const T& x) {}
+  template <class T> inline constexpr T log(const T& x, const T& base) { return log2(x)/log2(base); }
+  inline uint32_t log2(uint32_t x) { return bsr(x); }
+  inline uint32_t clog2(uint32_t x) { return bsr((x << 1) - 1); }
 
-  template <class T> T pmod(T x,const T& m) {
+  template <class T> inline constexpr const T& max(const T& a, const T& b) { return (a>b) ? a : b; }
+  template <class T> inline constexpr const T& min(const T& a, const T& b) { return (a<b) ? a : b; }
+  template <class T> inline constexpr const T& clamp(const T& v, const T& min, const T& max) { return (v<min) ? min : ((v>max) ? max : v); }
+  template <class T> inline constexpr T abs(const T& n) { return (n<0) ? -n : n; }
+  template <class T> inline constexpr T sqr(const T& x) { return x*x; }
+  inline constexpr uint32_t bitmask(uint32_t bits) { return ~(~0<<bits); }
+
+  template <class T> T pmod(T x, const T& m) {
     while(x>=m)x -= m;
     while(x<0)x += m;
     return x;
   }
-  template <> inline int pmod(int x,const int& m) {
+  template <> inline int pmod(int x, const int& m) {
     int wtr(x%m);
     return (wtr<0) ? wtr+m : wtr;
   }
 
-  template <class T> T lcd(const T& a,const T& b) {
-    T c(a),d(b);
+  template <class T> T lcd(const T& a, const T& b) {
+    T c(a), d(b);
     while(c!=d) {
       if(c<d) c += a;
       else d += b;
     }
     return c;
   }
-  template <class T> T gcd(T a,T b) {
-    if(a<b) swap(a,b);
+  template <class T> T gcd(T a, T b) {
+    if(a<b) swap(a, b);
     if(b!=0) {
       T c;
       while((b!=1)&&(a%b!=0)) {
@@ -45,20 +48,20 @@ namespace L {
       return b;
     } else return a;
   }
-  template <class T> T pow(const T& a,T b) {
+  template <class T> T pow(const T& a, T b) {
     T wtr(1);
     while(--b>=0) wtr *= a;
     return wtr;
   }
-  template <> inline float pow(const float& a,float b) { return ::pow(a,b); }
-  template <> inline double pow(const double& a,double b) { return ::pow(a,b); }
+  template <> inline float pow(const float& a, float b) { return ::pow(a, b); }
+  template <> inline double pow(const double& a, double b) { return ::pow(a, b); }
 
   template <typename T>
-  constexpr T orright(T v,int i){
-    return (i) ? (v|orright(v>>1,i-1)) : 0;
+  constexpr T orright(T v, int i) {
+    return (i) ? (v|orright(v>>1, i-1)) : 0;
   }
   template <typename T>
-  constexpr T upperpow2(T v){ // Returns next upper power of 2
-    return orright(v-1,sizeof(T)*8)+1;
+  constexpr T upperpow2(T v) { // Returns next upper power of 2
+    return orright(v-1, sizeof(T)*8)+1;
   }
 }
