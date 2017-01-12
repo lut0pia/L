bdf11f4479d82cec596a28764d25ab036fa2d53f
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-12 22:51:48 +0100

    Array starts with 16 bytes minimum

--- src/containers/Array.h
+++ src/containers/Array.h
@@ -105,8 +105,8 @@ namespace L {
     }
     void growTo(size_t size) {
       if(size>capacity()) {
-        // Avoid too low start (8 bytes min) and target power of two (byte-wise)
-        size_t tmp((_capacity) ? _capacity : (max<size_t>(8u,upperpow2(sizeof(T)))/sizeof(T)));
+        // Avoid too low start (16 bytes min) and target power of two (byte-wise)
+        size_t tmp((_capacity) ? _capacity : (max<size_t>(16u,upperpow2(sizeof(T)))/sizeof(T)));
         while(size>tmp) tmp *= 2;
         capacity(tmp);
       }
