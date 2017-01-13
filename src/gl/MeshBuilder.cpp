c54fad681410782acc0486dc9fcb0a78c198f141
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-14 00:48:31 +0100

    Added MeshBuilder::computeNormals again

--- src/gl/MeshBuilder.cpp
+++ src/gl/MeshBuilder.cpp
@@ -1,21 +1,41 @@
 #include "MeshBuilder.h"
 
+#include "../math/Vector.h"
+
 using namespace L;
 using namespace GL;
 
-void MeshBuilder::reset(){
+void MeshBuilder::reset() {
   _vertices.clear();
   _indices.clear();
   _vertexCount = 0;
 }
-void MeshBuilder::addVertex(const void* vertex,size_t size){
+void MeshBuilder::addVertex(const void* vertex, size_t size) {
   uintptr_t index(0);
-  while(index<_vertexCount && memcmp(&_vertices[size*index],vertex,size))
+  while(index<_vertexCount && memcmp(&_vertices[size*index], vertex, size))
     index++;
-  if(index==_vertexCount){
+  if(index==_vertexCount) {
     _vertices.size(_vertices.size()+size);
-    memcpy(&_vertices[_vertexCount*size],vertex,size);
+    memcpy(&_vertices[_vertexCount*size], vertex, size);
     _vertexCount++;
   }
   _indices.push(index);
+}
+void MeshBuilder::computeNormals(uint32_t vertexOffset, uint32_t normalOffset, size_t vertexSize) {
+#define ATTRIBUTE_OF(i,offset) (*(Vector3f*)(_vertices.begin()+vertexSize*i+offset))
+#define VERTEX_OF(i) ATTRIBUTE_OF(i,vertexOffset)
+#define NORMAL_OF(i) ATTRIBUTE_OF(i,normalOffset)
+  for(uint32_t i(0); i<_vertexCount; i++)
+    NORMAL_OF(i) = Vector3f(0, 0, 0);
+  for(uint32_t i(0); i<_indices.size(); i += 3) {
+    const Vector3f& a(VERTEX_OF(_indices[i]));
+    Vector3f n((VERTEX_OF(_indices[i+1])-a)
+               .cross(VERTEX_OF(_indices[i+2])-a));
+    n.normalize();
+    NORMAL_OF(_indices[i]) += n;
+    NORMAL_OF(_indices[i+1]) += n;
+    NORMAL_OF(_indices[i+2]) += n;
+  }
+  for(uint32_t i(0); i<_vertexCount; i++)
+    NORMAL_OF(i).normalize();
 }
--- src/gl/MeshBuilder.h
+++ src/gl/MeshBuilder.h
@@ -14,12 +14,13 @@ namespace L {
     public:
       constexpr MeshBuilder() : _vertexCount(0) {}
       void reset();
-      void addVertex(const void* vertex,size_t size);
-      inline const void* vertices()const{ return &_vertices[0]; }
-      inline const GLushort* indices()const{ return &_indices[0]; }
-      inline size_t verticesSize()const{ return _vertices.size(); }
-      inline size_t indexCount()const{ return _indices.size(); }
-      inline size_t vertexCount()const{ return _vertexCount; }
+      void addVertex(const void* vertex, size_t size);
+      void computeNormals(uint32_t vertexOffset, uint32_t normalOffset, size_t vertexSize); // Computes normals from vertices
+      inline const void* vertices() const { return &_vertices[0]; }
+      inline const GLushort* indices() const { return &_indices[0]; }
+      inline size_t verticesSize() const { return _vertices.size(); }
+      inline size_t indexCount() const { return _indices.size(); }
+      inline size_t vertexCount() const { return _vertexCount; }
     };
   }
 }
