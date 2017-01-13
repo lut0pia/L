46e1edb045ecbd05350feb561f726b05d05fd133
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-14 00:53:03 +0100

    Obj interface supports quads and missing normals

--- src/interface/obj.h
+++ src/interface/obj.h
@@ -4,59 +4,58 @@
 
 namespace L {
   class OBJ : public Interface<GL::Mesh> {
+  private:
+    typedef struct {
+      Vector3f _vertex;
+      Vector2f _uv;
+      Vector3f _normal;
+    } Vertex;
+    Array<Vector3f> _vertices, _normals;
+    Array<Vector2f> _uvs;
+    GL::MeshBuilder _mb;
   public:
     OBJ() : Interface("obj") {}
 
-    bool from(GL::Mesh& mesh,const File& f) {
-      static Array<Vector3f> vertices,normals;
-      static Array<Vector2f> texcoords;
-      static GL::MeshBuilder mb;
-      mb.reset();
-      vertices.clear();
-      normals.clear();
-      texcoords.clear();
-      FileStream fs(f.path(),"rb");
-      while(!fs.end()) {
-        String line(fs.line());
-        Array<String> linePart(line.explode(' '));
-        if(linePart.empty())
-          continue;
-        else if(linePart[0]=="v")
-          vertices.push(atof(linePart[1]),
-                        atof(linePart[2]),
-                        atof(linePart[3]));
-        else if(linePart[0]=="vt")
-          texcoords.push(atof(linePart[1]),
-                         1.f-atof(linePart[2])); // OpenGL has y going bottom up
-        else if(linePart[0]=="vn")
-          normals.push(atof(linePart[1]),
-                       atof(linePart[2]),
-                       atof(linePart[3]));
-        else if(linePart[0]=="f")
-          for(uintptr_t i(1); i<linePart.size(); i++){
-            Array<String> indices(linePart[i].explode('/'));
-            byte vertex[256];
-            size_t size(0);
-            int vi(atoi(indices[0])-1);
-            memcpy(vertex+size,&vertices[vi],sizeof(Vector3f));
-            size += sizeof(Vector3f);
-            if(indices.size()>1){
-              int ti(atoi(indices[1])-1);
-              memcpy(vertex+size,&texcoords[ti],sizeof(Vector2f));
-              size += sizeof(Vector2f);
+    bool from(GL::Mesh& mesh, Stream& stream) {
+      bool hasNormals(false);
+      _mb.reset();
+      _vertices.clear();
+      _normals.clear();
+      _uvs.clear();
+      while(!stream.end()) {
+        const Array<String> line(String(stream.line()).explode(' '));
+        if(line.empty()) continue;
+        else if(line[0]=="v") _vertices.push(atof(line[1]), atof(line[2]), atof(line[3]));
+        else if(line[0]=="vt") _uvs.push(atof(line[1]), 1.f-atof(line[2])); // OpenGL has y going bottom up
+        else if(line[0]=="vn") _normals.push(atof(line[1]), atof(line[2]), atof(line[3]));
+        else if(line[0]=="f") {
+          Vertex vertex, firstVertex, lastVertex;
+          for(uintptr_t i(1); i<line.size(); i++) {
+            const Array<String> indices(line[i].explode('/'));
+
+            switch(indices.size()) {
+              case 3: vertex._normal = _normals[atoi(indices[2])-1];
+                hasNormals = true;
+              case 2: vertex._uv = _uvs[atoi(indices[1])-1];
+              case 1: vertex._vertex = _vertices[atoi(indices[0])-1];
             }
-            if(indices.size()>2){
-              int ni(atoi(indices[2])-1);
-              memcpy(vertex+size,&normals[ni],sizeof(Vector3f));
-              size += sizeof(Vector3f);
+
+            if(i==1) firstVertex = vertex;
+            else if(i>3) {
+              _mb.addVertex(&firstVertex, sizeof(Vertex));
+              _mb.addVertex(&lastVertex, sizeof(Vertex));
             }
-            mb.addVertex(vertex,size);
+            _mb.addVertex(&vertex, sizeof(Vertex));
+            lastVertex = vertex;
           }
+        }
       }
-      mesh.load(mb,GL_TRIANGLES,{
-        GL::Mesh::Attribute{0,3,GL_FLOAT,GL_FALSE,sizeof(float)*8,0},
-        GL::Mesh::Attribute{1,2,GL_FLOAT,GL_FALSE,sizeof(float)*8,sizeof(float)*3},
-        GL::Mesh::Attribute{2,3,GL_FLOAT,GL_FALSE,sizeof(float)*8,sizeof(float)*5},
+      if(!hasNormals)
+        _mb.computeNormals(0, sizeof(Vector2f)+sizeof(Vector3f), sizeof(Vertex));
+      mesh.load(_mb, GL_TRIANGLES, {
+        {0,3,GL_FLOAT,GL_FALSE,sizeof(Vertex),0},
+        {1,2,GL_FLOAT,GL_FALSE,sizeof(Vertex),sizeof(float)*3},
+        {2,3,GL_FLOAT,GL_FALSE,sizeof(Vertex),sizeof(float)*5},
       });
       return true;
     }
