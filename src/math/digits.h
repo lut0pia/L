ea8fc5285c541652b21b3d8c31fa5d5d4d80f647
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-27 22:59:09 +0100

    Refactored digits
    Added support for NaN, more flexibility and smaller memory footprint

--- src/math/digits.h
+++ src/math/digits.h
@@ -4,34 +4,43 @@
 
 namespace L {
   // Describes the best integer type to convert T to (used for stringification)
-  template <class T> struct inttype{ typedef int32_t type; };
-  template <> struct inttype<int64_t>{ typedef int64_t type; };
-  template <> struct inttype<float>{ typedef int64_t type; };
-  template <> struct inttype<uint32_t>{ typedef uint32_t type; };
-  template <> struct inttype<uint64_t>{ typedef uint64_t type; };
+  template <class T> struct inttype { typedef int32_t type; };
+  template <> struct inttype<int64_t> { typedef int64_t type; };
+  template <> struct inttype<float> { typedef int64_t type; };
+  template <> struct inttype<uint32_t> { typedef uint32_t type; };
+  template <> struct inttype<uint64_t> { typedef uint64_t type; };
 
-  template <int base = 10,class T>
-  const char* ntos(T v,int pad = 0) { // Converts a number to a string
+  inline char* ntos_buffer() {
     static char buffer[128] = {0};
-    char* wtr(buffer+127);
-    // Check for negativity
-    bool negative;
-    if(v<0) {
-      negative = true;
-      v = -v;
-    } else negative = false;
-    // Check for point
-    int point(0);
+    return buffer+127;
+  }
+
+  template <int base, class T>
+  int ntos_point(T& v) {
+    int wtr(0);
     while(T((typename inttype<T>::type)v)!=v) {
       v *= base;
-      point++;
-      if(point<0) break;
+      wtr++;
+      if(wtr<0) break;
     }
+    return wtr;
+  }
+
+  template <class T> inline bool ntos_valid(const T& v) { return true; }
+  inline bool ntos_valid(float v) { return !isnan(v); }
+
+  template <int base = 10, class T>
+  const char* ntos(T v, int pad = 0) { // Converts a number to a string
+    if(!ntos_valid(v)) return "NaN"; // Check validity
+    char* wtr(ntos_buffer()); // Get buffer (backwards writing)
+    const bool negative(v<0); // Check negativity
+    if(negative) v = -v;
+
+    int point(ntos_point<base>(v)); // Check for point
     typename inttype<T>::type i(v); // Convert to int
 
     // Stringify
-    if(!i)
-      do{ *--wtr = '0'; } while(--pad>0);
+    if(!i) do { *--wtr = '0'; } while(--pad>0);
     else {
       while(pad>0 || i>0 || point>0) {
         char c('0'+(i%base));
@@ -45,8 +54,9 @@ namespace L {
     }
     return wtr;
   }
-  template <int base = 10> inline const char* ntos(double v,int pad = 0) { return ntos<base,float>((float)v,pad); }
-  template <int base = 10,class T>
+  template <int base = 10> inline const char* ntos(double v, int pad = 0) { return ntos<base, float>((float)v, pad); }
+
+  template <int base = 10, class T>
   T ston(const char* str) { // Converts a string to a number
     T wtr(0);
     bool negative(*str=='-' && *++str);
