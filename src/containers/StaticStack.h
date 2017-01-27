79c8786c46035c543a39bda0ff3a28869089106d
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-27 19:11:08 +0100

    Refactored StaticStack
    No longer using internal pointer

--- src/containers/StaticStack.h
+++ src/containers/StaticStack.h
@@ -4,39 +4,36 @@
 #include "../types.h"
 
 namespace L {
-  template <int n,class T>
+  template <int n, class T>
   class StaticStack {
   private:
     byte _array[n*sizeof(T)];
-    T* _current;
+    uintptr_t _size;
 
   public:
-    StaticStack() : _current(((T*)_array)-1) {}
+    constexpr StaticStack() : _array(), _size(0) {}
+    inline ~StaticStack() { while(!empty()) pop(); }
     template <class... Args>
     inline void push(Args&&... args) {
-      _current++;
-      L_ASSERT(_current<(T*)_array+n);
-      new(_current)T(args...);
+      L_ASSERT(_size<n);
+      _size++;
+      ::new(&top())T(args...);
     }
     inline void pop() {
-      L_ASSERT(_current>=(T*)_array);
-      _current->~T();
-      _current--;
+      L_ASSERT(_size>0);
+      top().~T();
+      _size--;
     }
-    inline T& top() { return *_current; }
-    inline const T& top() const { return *_current; }
-    inline T& top(int i){ return *(_current-i); }
-    inline const T& top(int i) const { return *(_current-i); }
-    inline T& bottom() { return *(T*)_array; }
-    inline const T& bottom() const { return *(T*)_array; }
-    inline T& bottom(int i){ return *((T*)_array+i); }
-    inline const T& bottom(int i) const { return *((T*)_array+i); }
-    inline T& operator[](int i) { return *(_current-i); }
-    inline size_t size() const{ return _current+1-(const T*)_array; }
-    inline void size(size_t s){
+    inline T& top(uintptr_t i = 0) { return *((T*)_array+(_size-i-1)); }
+    inline const T& top(uintptr_t i = 0) const { return *((T*)_array+(_size-i-1)); }
+    inline T& bottom(uintptr_t i = 0) { return *((T*)_array+i); }
+    inline const T& bottom(uintptr_t i = 0) const { return *((T*)_array+i); }
+    inline T& operator[](uintptr_t i) { return top(i); }
+    inline size_t size() const { return _size; }
+    inline void size(size_t s) {
       while(s<size()) pop();
       while(s>size()) push();
     }
-    inline bool empty() const { return (const T*)_array==_current+1; }
+    inline bool empty() const { return size()==0; }
   };
 }
