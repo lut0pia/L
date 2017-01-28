ea6d87d7f9a3f6484409985b3d0368e472d6b6dc
Lucien Catonnet <lutopia@hotmail.fr>
2017-01-28 18:20:57 +0100

    Format and member initialization

--- src/system/Device.h
+++ src/system/Device.h
@@ -4,12 +4,12 @@
 #include "../containers/StaticRing.h"
 #include "../text/String.h"
 
-namespace L{
+namespace L {
   class Device {
   public:
-    struct Event{
+    struct Event {
       const Device* _device;
-      uint8_t _index : 7,_pressed : 1;
+      uint8_t _index : 7, _pressed : 1;
     };
     void* _id;
     String _name;
@@ -17,18 +17,18 @@ namespace L{
     float _axes[8];
   private:
     static Array<Device> _devices;
-    static StaticRing<64,Event> _events;
+    static StaticRing<64, Event> _events;
     void* _systemData;
   public:
-    inline Device() : _buttons(0){}
-    inline float axis(int i) const{ return _axes[i]; }
-    inline bool button(int i) const{ return (_buttons & (1<<i)) != 0; }
+    inline Device() : _buttons(0), _axes{} {}
+    inline float axis(int i) const { return _axes[i]; }
+    inline bool button(int i) const { return (_buttons & (1<<i)) != 0; }
 
-    static const Array<Device>& devices(){ return _devices; }
+    static const Array<Device>& devices() { return _devices; }
     static const Device& device(void* id);
     static void init();
     static void update();
-    static void processReport(void* id,const byte* data,size_t size);
+    static void processReport(void* id, const byte* data, size_t size);
     static bool newEvent(Event&);
   };
 }
