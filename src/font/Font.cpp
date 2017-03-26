1503f23ca44ac0cf6a5e3f9689b767a43499aa6c
Lucien Catonnet <lutopia@hotmail.fr>
2017-03-26 13:14:09 +0200

    Font uses Table

--- src/font/Font.cpp
+++ src/font/Font.cpp
@@ -5,11 +5,14 @@
 
 using namespace L;
 
-Map<uint32_t,Ref<Font::Base> > fonts;
+Table<uint32_t, Ref<Font::Base> > fonts;
 
 Font::Base& Font::get(const char* name) {
   static Font::Pixel pixel;
-  return (fonts.has(fnv1a(name)))?*fonts[fnv1a(name)]:pixel;
+  const uint32_t h(fnv1a(name));
+  if(auto font = fonts.find(h))
+    return **font;
+  else return pixel;
 }
 void Font::set(const Ref<Base>& font, const char* name) {
   fonts[fnv1a(name)] = font;
