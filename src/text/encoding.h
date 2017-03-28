576b2c9e18cc277b0c5d01ea7f980388b100ae3c
Lucien Catonnet <lutopia@hotmail.fr>
2017-03-28 23:40:02 +0200

    Added ReadUTF8

--- src/text/encoding.h
+++ src/text/encoding.h
@@ -7,6 +7,22 @@
 namespace L {
   const char* UTF16toUTF8(uint16_t); // Returns UTF-8 string from UTF-16 char
   uint32_t UTF8toUTF32(const char* str, int* size); // Returns UTF-32 char (s being UTF-8) and puts its size in size
+  inline uint32_t ReadUTF8(const char*& str) { // Returns code point for pointed character and increments pointer to next character
+    uint32_t wtr((byte)*str);
+    if(wtr>>5==0x6) { // 2 bytes
+      wtr <<= 6;
+      wtr |= ((byte)*(++str)) & bitmask(6);
+      wtr &= bitmask(11);
+    } else if(wtr>>4==0xE) { // 3 bytes
+      wtr <<= 6;
+      wtr |= ((byte)*(++str)) & bitmask(6);
+      wtr <<= 6;
+      wtr |= ((byte)*(++str)) & bitmask(6);
+      wtr &= bitmask(16);
+    }
+    str++;
+    return wtr;
+  }
   Array<uint32_t> UTF8toUTF32(const char* str);
   String ANSItoUTF8(String); // Convert ANSI characters to UTF-8
 }
