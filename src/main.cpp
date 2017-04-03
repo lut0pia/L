d68f73b0dfca277a299582dc1ec3d164c7e0a796
Lucien Catonnet <lutopia@hotmail.fr>
2017-04-03 21:47:36 +0200

    Add ScriptComponent's gui to the engine

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ int main(int argc,const char* argv[]) {
   Engine::addSubUpdate<RigidBody>();
   Engine::addSubUpdate<Collider>();
   Engine::addLateUpdate<ScriptComponent>();
+  Engine::addGui<ScriptComponent>();
   {
     ScriptComponent::init();
     Script::Context startupContext;
