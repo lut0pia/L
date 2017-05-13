d9dcb121f6c1f784a9f1fd35a44507011091d220
Lucien Catonnet <lutopia@hotmail.fr>
2017-05-13 21:03:38 +0200

    Added Stream << unsigned long long

--- src/streams/Stream.h
+++ src/streams/Stream.h
@@ -37,6 +37,7 @@ namespace L {
     inline Stream& operator<<(int v) { return *this << ntos(v); }
     inline Stream& operator<<(unsigned int v) { return *this << ntos(v); }
     inline Stream& operator<<(long long v) { return *this << ntos(v); }
+    inline Stream& operator<<(unsigned long long v) { return *this << ntos(v); }
     inline Stream& operator<<(float v) { return *this << ntos(v); }
     inline Stream& operator<<(double v) { return *this << ntos(v); }
 
