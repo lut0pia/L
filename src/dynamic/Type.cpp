c598b7d14514584d10dd18dddf9da441c1fa87af
Lucien Catonnet <lutopia@hotmail.fr>
2017-07-02 22:22:48 +0200

    Formatting

--- src/dynamic/Type.cpp
+++ src/dynamic/Type.cpp
@@ -10,12 +10,12 @@ using namespace L;
 
 Table<Symbol, const TypeDescription*> L::types;
 
-Cast TypeDescription::cast(const TypeDescription* target) const{
+Cast TypeDescription::cast(const TypeDescription* target) const {
   auto found = casts.find((intptr_t)target);
   return found ? *found : nullptr;
 }
 
-void L::TypeInit(){
+void L::TypeInit() {
   // Operators
   Type<uint8_t>::canall<>();
   Type<int8_t>::canall<>();
@@ -39,10 +39,10 @@ void L::TypeInit(){
   Type<Vector3f>::canmath();
 
   // Casts
-  Type<int>::addcast<bool>([](void* dst,const void* src){new(dst)bool((*(int*)src)!=0); });
+  Type<int>::addcast<bool>([](void* dst, const void* src) {new(dst)bool((*(int*)src)!=0); });
   Type<int>::addcast<float>();
-  Type<int>::addcast<String>([](void* dst,const void* src){new(dst)String(ntos(*(int*)src)); });
+  Type<int>::addcast<String>([](void* dst, const void* src) {new(dst)String(ntos(*(int*)src)); });
   Type<unsigned int>::addcast<float>();
-  Type<float>::addcast<String>([](void* dst,const void* src){new(dst)String(ntos(*(float*)src)); });
-  Type<String>::addcast<bool>([](void* dst,const void* src){new(dst)bool(!((String*)src)->empty()); });
+  Type<float>::addcast<String>([](void* dst, const void* src) {new(dst)String(ntos(*(float*)src)); });
+  Type<String>::addcast<bool>([](void* dst, const void* src) {new(dst)bool(!((String*)src)->empty()); });
 }
