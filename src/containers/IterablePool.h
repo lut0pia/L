91e4bd939a807519207899144b56e09742f5d244
Lucien Catonnet <lutopia@hotmail.fr>
2017-07-08 00:07:25 +0200

    Added IterablePool

---
+++ src/containers/IterablePool.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Array.h"
+#include "Pool.h"
+
+namespace L {
+  template<class T>
+  class IterablePool {
+  protected:
+    Array<T*> _objects;
+    Pool<T> _pool;
+
+  public:
+    T* allocate() {
+      T* object(_pool.allocate());
+      _objects.push(object);
+      return object;
+    }
+    void deallocate(T* p) {
+      _pool.deallocate(p);
+      const uintptr_t i = _objects.find(p);
+      if(i!=-1)
+        _objects.erase_fast(i);
+    }
+    const Array<T*>& objects() const { return _objects; }
+  };
+}
