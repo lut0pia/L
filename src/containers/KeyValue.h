46a46298d2fc5e6dd774c81e2f126d86ab416e7e
Lucien Catonnet <lutopia@hotmail.fr>
2017-07-19 22:10:37 +0200

    KeyValue no longer requires key to have operator >

--- src/containers/KeyValue.h
+++ src/containers/KeyValue.h
@@ -11,7 +11,7 @@ namespace L {
   public:
     KeyValue(const K& key = K(), const V& value = V()) : _key(key), _value(value) {}
     inline bool operator<(const KeyValue& other) const { return key()<other.key(); }
-    inline bool operator>(const KeyValue& other) const { return key()>other.key(); }
+    inline bool operator>(const KeyValue& other) const { return other.key()<key(); }
     inline bool operator==(const KeyValue& other) const { return key()==other.key(); }
     inline const K& key() const { return _key; }
     inline const V& value() const { return _value; }
