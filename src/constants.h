19fcb639828cb652fd0f7cbae02ad5249aa29e8a
Lucien Catonnet <lutopia@hotmail.fr>
2017-08-06 00:10:41 +0200

    Added constant e

--- src/constants.h
+++ src/constants.h
@@ -5,6 +5,10 @@ namespace L {
     static const T wtr((T)3.14159265358979323846);
     return wtr;
   }
+  template <class T> inline const T& E() {
+    static const T wtr((T)2.71828182845904523536);
+    return wtr;
+  }
   template <class T> inline const T& DEGTORAD() {
     static const T wtr(PI<T>()/(T)180);
     return wtr;
