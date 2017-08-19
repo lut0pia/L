82c6507ab6b6821d4fc6979f417fe7dcab1a09f4
Lucien Catonnet <lutopia@hotmail.fr>
2017-08-20 00:37:56 +0200

    Added shader source loading with lengths

--- src/gl/Shader.cpp
+++ src/gl/Shader.cpp
@@ -5,31 +5,27 @@
 using namespace L;
 using namespace GL;
 
-void Shader::load(const char* src) {
+void Shader::load(const char** src, uint32_t count, GLint* lengths) {
   GLint compile_status(GL_TRUE);
-  glShaderSource(_id,1,(const GLchar**)&src,nullptr);
+  glShaderSource(_id, count, (const GLchar**)src, lengths);
   glCompileShader(_id);
-  glGetShaderiv(_id,GL_COMPILE_STATUS,&compile_status);
+  glGetShaderiv(_id, GL_COMPILE_STATUS, &compile_status);
   if(compile_status != GL_TRUE) {
     GLchar buffer[2048];
     GLsizei count;
-    glGetShaderInfoLog(_id,sizeof(buffer),&count,buffer);
+    glGetShaderInfoLog(_id, sizeof(buffer), &count, buffer);
     glDeleteShader(_id);
-    L_ERRORF("Couldn't compile shader: %.*s",count,buffer);
+    L_ERRORF("Couldn't compile shader: %.*s", count, buffer);
   }
 }
-Shader::Shader(File file,GLenum type) : _id(glCreateShader(type)) {
-  CFileStream fs(file.path(),"rb");
-  String src;
-  while(!fs.end()) {
-    char c(fs.get());
-    if(c==EOF) break;
-    else src.push(c);
-  }
+Shader::Shader(const char* src, GLenum type) : _id(glCreateShader(type)) {
   load(src);
 }
-Shader::Shader(const char* src,GLenum type) : _id(glCreateShader(type)) {
-  load(src);
+Shader::Shader(const char** src, uint32_t count, GLenum type) : _id(glCreateShader(type)) {
+  load(src, count);
+}
+Shader::Shader(const char** src, GLint* lengths, uint32_t count, GLenum type) : _id(glCreateShader(type)) {
+  load(src, count, lengths);
 }
 Shader::~Shader() {
   glDeleteShader(_id);
--- src/gl/Shader.h
+++ src/gl/Shader.h
@@ -10,10 +10,12 @@ namespace L {
       L_NOCOPY(Shader)
     private:
       GLuint _id;
-      void load(const char* src);
+      void load(const char** src, uint32_t count, GLint* lengths = nullptr);
+      inline void load(const char* src) { load(&src, 1); }
     public:
-      Shader(File file,GLenum type);
-      Shader(const char* src,GLenum type);
+      Shader(const char* src, GLenum type);
+      Shader(const char** src, uint32_t count, GLenum type);
+      Shader(const char** src, GLint* lengths, uint32_t count, GLenum type);
       ~Shader();
       inline GLuint id() const { return _id; }
     };
