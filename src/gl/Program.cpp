3b1692543fe0d9bf8136314d7f33750c2871c06f
Lucien Catonnet <lutopia@hotmail.fr>
2017-08-20 12:21:00 +0200

    Program linking is more atomic

--- src/gl/Program.cpp
+++ src/gl/Program.cpp
@@ -6,63 +6,65 @@
 using namespace L;
 using namespace GL;
 
+Program::Program(const Shader* s, uint32_t count) : _id(glCreateProgram()) {
+  GLuint shaders[8];
+  L_ASSERT(count<=L_COUNT_OF(shaders));
+  for(uintptr_t i(0); i<count; i++)
+    shaders[i] = s[i].id();
+  link(shaders, count);
+}
 Program::Program(const Shader& s1) : _id(glCreateProgram()) {
-  attach(s1);
-  link();
-  detach(s1);
+  const GLuint shaders[] = {s1.id()};
+  link(shaders, 1);
 }
-Program::Program(const Shader& s1,const Shader& s2) : _id(glCreateProgram()) {
-  attach(s1);
-  attach(s2);
-  link();
-  detach(s1);
-  detach(s2);
+Program::Program(const Shader& s1, const Shader& s2) : _id(glCreateProgram()) {
+  const GLuint shaders[] = {s1.id(),s2.id()};
+  link(shaders, 2);
 }
-Program::Program(const Shader& s1,const Shader& s2,const Shader& s3) : _id(glCreateProgram()) {
-  attach(s1);
-  attach(s2);
-  attach(s3);
-  link();
-  detach(s1);
-  detach(s2);
-  detach(s3);
+Program::Program(const Shader& s1, const Shader& s2, const Shader& s3) : _id(glCreateProgram()) {
+  const GLuint shaders[] = {s1.id(),s2.id(),s3.id()};
+  link(shaders, 3);
 }
 Program::~Program() {
   glDeleteProgram(_id);
 }
-void Program::attach(const Shader& shader) {
-  glAttachShader(_id,shader.id());
-}
-void Program::detach(const Shader& shader) {
-  glDetachShader(_id,shader.id());
-}
 
-void Program::link() {
+void Program::link(const GLuint* shaders, uint32_t count) {
+  for(uintptr_t i(0); i<count; i++)
+    glAttachShader(_id, shaders[i]);
+
   glLinkProgram(_id);
   GLint p;
-  glGetProgramiv(_id,GL_LINK_STATUS,&p);
-  if(p != GL_TRUE)
-    L_ERROR("Couldn't link program");
+  glGetProgramiv(_id, GL_LINK_STATUS, &p);
+  if(p != GL_TRUE) {
+    GLchar buffer[2048];
+    GLsizei count;
+    glGetProgramInfoLog(_id, sizeof(buffer), &count, buffer);
+    L_ERRORF("Couldn't link program:\n%.*s", count, buffer);
+  }
+
+  for(uintptr_t i(0); i<count; i++)
+    glDetachShader(_id, shaders[i]);
 }
 void Program::use() const {
   glUseProgram(_id);
 }
 GLuint Program::uniformLocation(const char* name) {
-  KeyValue<uint32_t,GLuint>* it(_uniformLocation.find(fnv1a(name)));
+  KeyValue<uint32_t, GLuint>* it(_uniformLocation.find(fnv1a(name)));
   if(it) return it->value();
   else {
-    GLuint location(glGetUniformLocation(_id,name));
+    GLuint location(glGetUniformLocation(_id, name));
     L_ASSERT(location>=0);
     return _uniformLocation[fnv1a(name)] = location;
   }
 }
-GLuint Program::uniformBlockIndex(const char* name){
-  GLuint wtr(glGetUniformBlockIndex(_id,name));
+GLuint Program::uniformBlockIndex(const char* name) {
+  GLuint wtr(glGetUniformBlockIndex(_id, name));
   L_ASSERT(wtr != GL_INVALID_INDEX);
   return wtr;
 }
-void Program::uniformBlockBinding(const char* name,GLuint binding){
-  glUniformBlockBinding(_id,uniformBlockIndex(name),binding);
+void Program::uniformBlockBinding(const char* name, GLuint binding) {
+  glUniformBlockBinding(_id, uniformBlockIndex(name), binding);
 }
 void Program::uniform(const char* name, int v) {
   glUniform1i(uniformLocation(name), v);
@@ -70,23 +72,23 @@ void Program::uniform(const char* name, int v) {
 void Program::uniform(const char* name, unsigned int v) {
   glUniform1ui(uniformLocation(name), v);
 }
-void Program::uniform(const char* name,float v) {
-  glUniform1f(uniformLocation(name),v);
+void Program::uniform(const char* name, float v) {
+  glUniform1f(uniformLocation(name), v);
 }
 void Program::uniform(const char* name, float x, float y) {
   glUniform2f(uniformLocation(name), x, y);
 }
-void Program::uniform(const char* name,float x,float y,float z) {
-  glUniform3f(uniformLocation(name),x,y,z);
+void Program::uniform(const char* name, float x, float y, float z) {
+  glUniform3f(uniformLocation(name), x, y, z);
 }
-void Program::uniform(const char* name,float x,float y,float z,float w) {
-  glUniform4f(uniformLocation(name),x,y,z,w);
+void Program::uniform(const char* name, float x, float y, float z, float w) {
+  glUniform4f(uniformLocation(name), x, y, z, w);
 }
-void Program::uniform(const char* name,const Matrix44f& m) {
-  glUniformMatrix4fv(uniformLocation(name),1,GL_FALSE,m.array());
+void Program::uniform(const char* name, const Matrix44f& m) {
+  glUniformMatrix4fv(uniformLocation(name), 1, GL_FALSE, m.array());
 }
-void Program::uniform(const char* name,const Texture& texture,GLenum unit) {
-  glUniform1i(uniformLocation(name),unit-GL_TEXTURE0);
+void Program::uniform(const char* name, const Texture& texture, GLenum unit) {
+  glUniform1i(uniformLocation(name), unit-GL_TEXTURE0);
   glActiveTexture(unit);
   texture.bind();
 }
--- src/gl/Program.h
+++ src/gl/Program.h
@@ -13,31 +13,30 @@ namespace L {
       L_NOCOPY(Program)
     private:
       GLuint _id;
-      Map<uint32_t,GLuint> _uniformLocation;
-      void attach(const Shader&);
-      void detach(const Shader&);
-      void link();
+      Map<uint32_t, GLuint> _uniformLocation;
     public:
+      Program(const Shader*, uint32_t);
       Program(const Shader&);
-      Program(const Shader&,const Shader&);
-      Program(const Shader&,const Shader&,const Shader&);
+      Program(const Shader&, const Shader&);
+      Program(const Shader&, const Shader&, const Shader&);
       ~Program();
+      void link(const GLuint*, uint32_t count);
       void use() const;
       GLuint uniformLocation(const char* name);
       GLuint uniformBlockIndex(const char* name);
-      void uniformBlockBinding(const char* name,GLuint binding);
+      void uniformBlockBinding(const char* name, GLuint binding);
       void uniform(const char* name, int);
       void uniform(const char* name, unsigned int);
-      void uniform(const char* name,float);
+      void uniform(const char* name, float);
       void uniform(const char* name, float, float);
-      void uniform(const char* name,float,float,float);
-      void uniform(const char* name,float,float,float,float);
+      void uniform(const char* name, float, float, float);
+      void uniform(const char* name, float, float, float, float);
       inline void uniform(const char* name, const Vector2f& v) { uniform(name, v.x(), v.y()); }
-      inline void uniform(const char* name,const Vector3f& v) { uniform(name,v.x(),v.y(),v.z()); }
-      inline void uniform(const char* name,const Vector4f& v) { uniform(name,v.x(),v.y(),v.z(),v.w()); }
-      inline void uniform(const char* name,const Color& c) { uniform(name,c.rf(),c.gf(),c.bf(),c.af()); }
-      void uniform(const char* name,const Matrix44f&);
-      void uniform(const char* name,const Texture&,GLenum unit = GL_TEXTURE0);
+      inline void uniform(const char* name, const Vector3f& v) { uniform(name, v.x(), v.y(), v.z()); }
+      inline void uniform(const char* name, const Vector4f& v) { uniform(name, v.x(), v.y(), v.z(), v.w()); }
+      inline void uniform(const char* name, const Color& c) { uniform(name, c.rf(), c.gf(), c.bf(), c.af()); }
+      void uniform(const char* name, const Matrix44f&);
+      void uniform(const char* name, const Texture&, GLenum unit = GL_TEXTURE0);
 
       static void unuse();
     };
