d3be1e3e4bd7ecaa0cf52c8e15fa96eeb324611a
Lucien Catonnet <lutopia@hotmail.fr>
2017-09-09 22:39:01 +0200

    Sprite uses GL::draw instead of custom mesh

--- src/engine/Sprite.cpp
+++ src/engine/Sprite.cpp
@@ -17,44 +17,23 @@ void Sprite::unpack(const Map<Symbol, Var>& data) {
   unpack_item(data, "texture", _texture);
   unpack_item(data, "vertex", _vertex);
   unpack_item(data, "uv", _uv);
-  update_mesh();
 }
 
-void Sprite::vertex(const Interval2f& v) {
-  _vertex = v;
-  update_mesh();
-}
-void Sprite::uv(const Interval2f& u) {
-  _uv = u;
-  update_mesh();
-}
-void Sprite::update_mesh() {
-  const GLfloat quad[] = {
-    _vertex.min().x(),_vertex.min().y(),
-    _uv.min().x(),_uv.max().y(),
-    _vertex.max().x(),_vertex.min().y(),
-    _uv.max().x(),_uv.max().y(),
-    _vertex.min().x(),_vertex.max().y(),
-    _uv.min().x(),_uv.min().y(),
-    _vertex.max().x(),_vertex.max().y(),
-    _uv.max().x(),_uv.min().y(),
-  };
-  _mesh.load(GL_TRIANGLE_STRIP, 4, quad, sizeof(quad), {
-    {0,2,GL_FLOAT,GL_FALSE,sizeof(float)*4,0},
-    {1,2,GL_FLOAT,GL_FALSE,sizeof(float)*4,sizeof(float)*2},
-  });
-}
 void Sprite::render(const Camera&) {
   static GL::Program program(GL::Shader(
     "#version 330 core\n"
-    "layout (location = 0) in vec2 vposition;"
-    "layout (location = 1) in vec2 vtexcoords;"
     L_SHAREDUNIFORM
+    "uniform vec4 vert;"
+    "uniform vec4 uv;"
     "uniform mat4 model;"
     "out vec2 ftexcoords;"
+    "out vec3 fnormal;"
+    "const int[] indices = int[](1,0,2,1,2,3);"
     "void main(){"
-    "ftexcoords = vtexcoords;"
-    "gl_Position = viewProj * model * vec4(vposition.x,0.f,vposition.y,1.f);"
+    "int index = indices[gl_VertexID];"
+    "ftexcoords = vec2(uv[index/2],uv[1-index%2+2]);"
+    "fnormal = mat3(model)*vec3(0,-1,0);"
+    "gl_Position = viewProj * model * vec4(vert[index/2],0.f,vert[index%2+2],1.f);"
     "}", GL_VERTEX_SHADER),
     GL::Shader(
       "#version 330 core\n"
@@ -62,19 +41,21 @@ void Sprite::render(const Camera&) {
       L_SHADER_LIB
       "layout(location = 0) out vec3 ocolor;"
       "layout(location = 1) out vec3 onormal;"
-      "uniform mat4 model;"
       "uniform sampler2D tex;"
       "in vec2 ftexcoords;"
+      "in vec3 fnormal;"
       "void main(){"
       "vec4 color = texture(tex,ftexcoords);"
       "if(alpha(color.a)) discard;"
       "ocolor = color.rgb;"
-      "onormal.xy = encodeNormal(mat3(model)*vec3(0,-1,0));"
+      "onormal.xy = encodeNormal(fnormal);"
       "}", GL_FRAGMENT_SHADER));
   if(_texture) {
     program.use();
     program.uniform("tex", *_texture);
     program.uniform("model", _transform->matrix());
-    _mesh.draw();
+    program.uniform("vert", Vector4f(_vertex.min().x(), _vertex.max().x(), _vertex.min().y(), _vertex.max().y()));
+    program.uniform("uv", Vector4f(_uv.min().x(), _uv.max().x(), _uv.min().y(), _uv.max().y()));
+    GL::draw(GL_TRIANGLES, 6);
   }
 }
--- src/engine/Sprite.h
+++ src/engine/Sprite.h
@@ -12,23 +12,19 @@ namespace L {
   private:
     Transform* _transform;
     Resource<GL::Texture> _texture;
-    GL::Mesh _mesh;
     Interval2f _vertex, _uv;
   public:
-    inline Sprite() : _vertex(Vector2f(-1.f, -1.f), Vector2f(1.f, 1.f)), _uv(Vector2f(0.f, 0.f), Vector2f(1.f, 1.f)) {
-      update_mesh();
-    }
+    inline Sprite() : _vertex(Vector2f(-1.f, -1.f), Vector2f(1.f, 1.f)), _uv(Vector2f(0.f, 0.f), Vector2f(1.f, 1.f)) {}
     inline Sprite(const Sprite&) : Sprite() { L_ERROR("Sprite component should not be copied."); }
     inline Sprite& operator=(const Sprite& other) { L_ERROR("Sprite component should not be copied."); }
 
     inline void updateComponents() override { _transform = entity()->requireComponent<Transform>(); }
     virtual Map<Symbol, Var> pack() const override;
     virtual void unpack(const Map<Symbol, Var>&) override;
+    void render(const Camera&);
 
     inline void texture(const char* filename) { _texture = Resource<GL::Texture>::get(filename); }
-    void vertex(const Interval2f& v);
-    void uv(const Interval2f& u);
-    void update_mesh();
-    void render(const Camera&);
+    inline void vertex(const Interval2f& v) { _vertex = v; }
+    inline void uv(const Interval2f& u) { _uv = u; }
   };
 }
