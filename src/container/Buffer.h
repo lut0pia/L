d161bdb1d9c1647c9fb07cbf6e6467997a32ddd3
Lucien Catonnet <lutopia@hotmail.fr>
2017-09-30 23:25:42 +0200

    Added Buffer::size method

--- src/container/Buffer.h
+++ src/container/Buffer.h
@@ -13,7 +13,9 @@ namespace L {
     inline ~Buffer() {
       if(_data) Memory::free(_data, _size);
     }
-    inline void* data() const { return _data; }
+    inline void* data() { return _data; }
+    inline const void* data() const { return _data; }
+    inline void size(size_t new_size) { _data = Memory::realloc(_data, _size, new_size); _size = new_size; }
     inline size_t size() const { return _size; }
     inline operator void*() const { return _data; }
   };
