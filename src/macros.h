566c456aae8934cdd922b4768d41b99fb98bc457
Lucien Catonnet <lutopia@hotmail.fr>
2018-02-07 22:56:14 +0100

    Added L_CONCAT macro

--- src/macros.h
+++ src/macros.h
@@ -9,6 +9,9 @@
 #define L_STRINGIFY(n) L_STRINGIFY_(n)
 #define L_STRINGIFY_(n) #n
 
+#define L_CONCAT(a,b) L_CONCAT_(a,b)
+#define L_CONCAT_(a,b) a##b
+
 #define L_ONCE do{static bool DONE_ONCE(false);if(DONE_ONCE) return;DONE_ONCE = true;}while(false)
 #define L_DO_ONCE static bool DONE_ONCE(false);if(!DONE_ONCE && (DONE_ONCE = true))
 
