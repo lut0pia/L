25c0078a6a9c59264fe94a7392f864ca2eb36d58
Lucien Catonnet <lutopia@hotmail.fr>
2018-05-19 12:32:48 +0200

    Parallel Primitive bounds computation

--- src/component/Primitive.cpp
+++ src/component/Primitive.cpp
@@ -23,19 +23,30 @@ void Primitive::script_registration() {
   L_COMPONENT_METHOD(Primitive, "scale", 1, scale(c.local(0).get<Vector3f>()));
 }
 
-void Primitive::late_update() {
-  const Interval3f model_bounds(_material.bounds());
-  Interval3f world_bounds(_transform->position());
-  for(uint32_t i(0); i<8; i++)
-    world_bounds.add(_transform->toAbsolute(Vector3f(
-      (i&1 ? model_bounds.min().x() : model_bounds.max().x())*_scale.x(),
-      (i&2 ? model_bounds.min().y() : model_bounds.max().y())*_scale.y(),
-      (i&4 ? model_bounds.min().z() : model_bounds.max().z())*_scale.z()
-    )));
-  _cull_volume.update_bounds(world_bounds);
-}
 void Primitive::render(const Camera& camera) {
   if(_cull_volume.visible() && _material.valid()) {
     _material.draw(_transform->matrix()*scale_matrix(_scale));
+  }
+}
+
+void Primitive::late_update_all() {
+  {
+    L_SCOPE_MARKER("Primitive bounds computation");
+    ComponentPool<Primitive>::async_iterate([](Primitive& c, uint32_t) {
+      const Interval3f model_bounds(c._material.bounds());
+      c._bounds = c._transform->position();
+      for(uint32_t i(0); i<8; i++)
+        c._bounds.add(c._transform->toAbsolute(Vector3f(
+          (i&1 ? model_bounds.min().x() : model_bounds.max().x())*c._scale.x(),
+          (i&2 ? model_bounds.min().y() : model_bounds.max().y())*c._scale.y(),
+          (i&4 ? model_bounds.min().z() : model_bounds.max().z())*c._scale.z()
+        )));
+    });
+  }
+  {
+    L_SCOPE_MARKER("Primitive cull volume update");
+    ComponentPool<Primitive>::iterate([](Primitive& c) {
+      c._cull_volume.update_bounds(c._bounds);
+    });
   }
 }
--- src/component/Primitive.h
+++ src/component/Primitive.h
@@ -9,10 +9,10 @@ namespace L {
   class Primitive : public Component {
     L_COMPONENT(Primitive)
       L_COMPONENT_HAS_RENDER(Primitive)
-      L_COMPONENT_HAS_LATE_UPDATE(Primitive)
   protected:
     Transform* _transform;
     CullVolume _cull_volume;
+    Interval3f _bounds;
     Material _material;
     Vector3f _scale = 1.f;
   public:
@@ -21,11 +21,12 @@ namespace L {
     virtual void unpack(const Map<Symbol, Var>&) override;
     static void script_registration();
     
-    void late_update();
     void render(const Camera& camera);
 
     inline void material(const char* filename) { _material.parent(Resource<Material>::get(filename)); }
     inline Material* material() { return &_material; }
     inline void scale(const Vector3f& s) { _scale = s; }
+
+    static void late_update_all();
   };
 }
