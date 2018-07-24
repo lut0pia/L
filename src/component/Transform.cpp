835cbe691e7d8c0d906adc7433a3899365627ff4
Lucien Catonnet <lutopia@hotmail.fr>
2018-07-24 22:14:30 +0200

    Added script method for Transform::toAbsolute

--- src/component/Transform.cpp
+++ src/component/Transform.cpp
@@ -18,6 +18,7 @@ void Transform::script_registration(){
   L_COMPONENT_RETURN_METHOD(Transform, "right", 0, right());
   L_COMPONENT_RETURN_METHOD(Transform, "forward", 0, forward());
   L_COMPONENT_RETURN_METHOD(Transform, "up", 0, up());
+  L_COMPONENT_RETURN_METHOD(Transform, "to-absolute", 1, toAbsolute(c.local(0)));
   L_COMPONENT_METHOD(Transform, "set-position", 1, position(c.local(0).get<Vector3f>()));
   L_COMPONENT_METHOD(Transform, "set-rotation", 2, rotation(c.local(0).get<Vector3f>(), c.local(1).get<float>()));
   L_COMPONENT_METHOD(Transform, "move", 1, move(c.local(0).get<Vector3f>()));
