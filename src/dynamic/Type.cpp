2b157baaffda35e10fb5224fb3b8bb7073bd3927
Lucien Catonnet <lutopia@hotmail.fr>
2018-08-10 11:50:45 +0200

    Added dynamic cast from float to uint64_t

--- src/dynamic/Type.cpp
+++ src/dynamic/Type.cpp
@@ -41,6 +41,7 @@ void L::TypeInit() {
   Type<int>::addcast<String>([](void* dst, const void* src) {new(dst)String(ntos(*(int*)src)); });
   Type<unsigned int>::addcast<float>();
   Type<float>::addcast<int>();
+  Type<float>::addcast<uint64_t>();
   Type<float>::addcast<String>([](void* dst, const void* src) {new(dst)String(ntos(*(float*)src)); });
   Type<float>::addcast<Vector3f>();
   Type<String>::addcast<bool>([](void* dst, const void* src) {new(dst)bool(!((String*)src)->empty()); });
