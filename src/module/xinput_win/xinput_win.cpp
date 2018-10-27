9c8722bcc27d5572560cecb6de19f395cfd9a294
Lucien Catonnet <lutopia@hotmail.fr>
2018-10-27 17:14:44 +0200

    Improved XInput performance by polling inactive devices less frequently

--- src/module/xinput_win/xinput_win.cpp
+++ src/module/xinput_win/xinput_win.cpp
@@ -1,6 +1,7 @@
 #include <L/src/container/Queue.h>
 #include <L/src/engine/Engine.h>
 #include <L/src/system/Device.h>
+#include <L/src/time/Timer.h>
 
 #include <Xinput.h>
 
@@ -11,14 +12,15 @@ XInputGetState_FN* DynamicXInputGetState;
 
 class XInputDevice : public Device {
 public:
-  DWORD user_index;
-  XINPUT_STATE state;
+  DWORD _user_index;
+  XINPUT_STATE _state;
+  Timer _inactive_timer;
 
   void set_name(const char* name) {
     _name = name;
   }
   inline void map_button(Device::Button b, DWORD xi_b) {
-    set_button(b, (state.Gamepad.wButtons & xi_b) != 0);
+    set_button(b, (_state.Gamepad.wButtons & xi_b) != 0);
   }
   inline void map_axis(Device::Axis a, SHORT xi_a) {
     _axes[size_t(a)] = float(xi_a)/32767.f;
@@ -27,8 +29,13 @@ public:
     _axes[size_t(a)] = float(xi_a)/255.f;
   }
   void update() {
-    if(DynamicXInputGetState(user_index, &state) == ERROR_SUCCESS) {
-      _active = true;
+    static const Time one_second(0, 0, 1);
+    // Only get state if active or every second if inactive
+    // Otherwise we may easily waste half a millisecond every frame
+    if(_active || _inactive_timer.every(one_second)) {
+      _active = DynamicXInputGetState(_user_index, &_state) == ERROR_SUCCESS;
+    }
+    if(_active) {
       apply_deadzones();
       map_button(Device::Button::GamepadLeftStick, XINPUT_GAMEPAD_LEFT_THUMB);
       map_button(Device::Button::GamepadRightStick, XINPUT_GAMEPAD_RIGHT_THUMB);
@@ -38,22 +45,20 @@ public:
       map_button(Device::Button::GamepadFaceRight, XINPUT_GAMEPAD_B);
       map_button(Device::Button::GamepadFaceLeft, XINPUT_GAMEPAD_X);
       map_button(Device::Button::GamepadFaceTop, XINPUT_GAMEPAD_Y);
-      map_axis(Axis::GamepadLeftStickX, state.Gamepad.sThumbLX);
-      map_axis(Axis::GamepadLeftStickY, state.Gamepad.sThumbLY);
-      map_axis(Axis::GamepadRightStickX, state.Gamepad.sThumbRX);
-      map_axis(Axis::GamepadRightStickY, state.Gamepad.sThumbRY);
-      map_axis(Axis::GamepadLeftTrigger, state.Gamepad.bLeftTrigger);
-      map_axis(Axis::GamepadRightTrigger, state.Gamepad.bRightTrigger);
-    } else {
-      _active = false;
+      map_axis(Axis::GamepadLeftStickX, _state.Gamepad.sThumbLX);
+      map_axis(Axis::GamepadLeftStickY, _state.Gamepad.sThumbLY);
+      map_axis(Axis::GamepadRightStickX, _state.Gamepad.sThumbRX);
+      map_axis(Axis::GamepadRightStickY, _state.Gamepad.sThumbRY);
+      map_axis(Axis::GamepadLeftTrigger, _state.Gamepad.bLeftTrigger);
+      map_axis(Axis::GamepadRightTrigger, _state.Gamepad.bRightTrigger);
     }
   }
   void apply_deadzones() {
 #define APPLY_DEADZONE(v, dz) if(abs<int32_t>(v)<=dz) {v = 0;}
-    APPLY_DEADZONE(state.Gamepad.sThumbLX, XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE);
-    APPLY_DEADZONE(state.Gamepad.sThumbLY, XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE);
-    APPLY_DEADZONE(state.Gamepad.sThumbRX, XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE);
-    APPLY_DEADZONE(state.Gamepad.sThumbRY, XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE);
+    APPLY_DEADZONE(_state.Gamepad.sThumbLX, XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE);
+    APPLY_DEADZONE(_state.Gamepad.sThumbLY, XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE);
+    APPLY_DEADZONE(_state.Gamepad.sThumbRX, XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE);
+    APPLY_DEADZONE(_state.Gamepad.sThumbRY, XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE);
 #undef APPLY_DEADZONE
   }
 };
@@ -81,7 +86,7 @@ void xinput_win_module_init() {
     char name[256];
     sprintf(name, "XPad%d", i+1);
     xinput_devices[i].set_name(name);
-    xinput_devices[i].user_index = DWORD(i);
+    xinput_devices[i]._user_index = DWORD(i);
   }
   Engine::add_parallel_update([]() {
     L_SCOPE_MARKER("XInput update");
