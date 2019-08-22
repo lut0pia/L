9bf44d96c60d1144719da8861d6b24a79df7e554
Lucien Catonnet <lutopia@hotmail.fr>
2019-08-22 19:24:45 +0200

    Using SOCKET in NetStream for consistency

--- src/stream/NetStream.h
+++ src/stream/NetStream.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "Stream.h"
+#include "../network/Network.h"
 
 namespace L {
   class NetStream : public Stream {
     protected:
-      int _sd;
+      SOCKET _sd;
     public:
-      NetStream(int sd) : _sd(sd) {}
+      NetStream(SOCKET sd) : _sd(sd) {}
       size_t write(const void* data, size_t size);
       size_t read(void* data, size_t size);
   };
