eca0708a3d933047d0f3dd78b32bf5fce757a7de
Lucien Catonnet <lutopia@hotmail.fr>
2019-08-23 16:54:27 +0200

    Only set DISABLE_VK_LAYER_VALVE_steam_overlay_1 under Windows

--- src/rendering/Vulkan.cpp
+++ src/rendering/Vulkan.cpp
@@ -69,7 +69,9 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT, VkDe
 }
 
 void Vulkan::init() {
-  putenv("DISABLE_VK_LAYER_VALVE_steam_overlay_1=1");
+#ifdef L_WINDOWS
+  _putenv("DISABLE_VK_LAYER_VALVE_steam_overlay_1=1");
+#endif
   L_SCOPE_MARKER("Vulkan::init");
   const char* validation_layers[] = {
     "VK_LAYER_LUNARG_standard_validation",
