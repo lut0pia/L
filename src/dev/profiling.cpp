f2c8f7c8e66c2cc05b556a3fc0ca877d7669da3f
Lucien Catonnet <lutopia@hotmail.fr>
2019-08-25 20:51:26 +0200

    Reuse profiling event buffer circularly

--- src/dev/profiling.cpp
+++ src/dev/profiling.cpp
@@ -15,26 +15,23 @@ struct ProfilingEvent {
   Time start, duration;
   uint32_t fiber_id;
 };
-ProfilingEvent events[2<<17];
+ProfilingEvent events[2 << 20];
 uint32_t event_index(0);
 
 ScopeMarker::~ScopeMarker() {
-  const Time duration(Time::now()-_start);
-  if(duration>0) {
+  const Time duration(Time::now() - _start);
+  if(duration > 0) {
     uint32_t index;
     do {
       index = event_index;
-      if(index>=L_COUNT_OF(events)) {
-        return;
-      }
-    } while(cas(&event_index, index, index+1)!=index);
+    } while(cas(&event_index, index, (index + 1) % L_COUNT_OF(events)) != index);
     events[index] = {_name, _start, duration, TaskSystem::fiber_id()};
   }
 }
 ScopeMarkerFormatted::ScopeMarkerFormatted(const char* format, ...) : ScopeMarker(nullptr) {
   va_list args;
   va_start(args, format);
-  const size_t size(vsnprintf(nullptr, 0, format, args)+1);
+  const size_t size(vsnprintf(nullptr, 0, format, args) + 1);
   _name = Memory::alloc_type<char>(size);
   va_start(args, format);
   vsnprintf((char*)_name, size, format, args);
@@ -45,16 +42,13 @@ struct ProfilingCounterEvent {
   Time time;
   int64_t value;
 };
-ProfilingCounterEvent counter_events[2<<17];
+ProfilingCounterEvent counter_events[2 << 20];
 uint32_t counter_event_index(0);
 void L::count_marker(const char* name, int64_t value) {
   uint32_t index;
   do {
     index = counter_event_index;
-    if(index>=L_COUNT_OF(events)) {
-      return;
-    }
-  } while(cas(&counter_event_index, index, index+1)!=index);
+  } while(cas(&counter_event_index, index, (index + 1) % L_COUNT_OF(counter_events)) != index);
   counter_events[index] = {name, Time::now(), value};
 }
 
@@ -62,11 +56,11 @@ void L::flush_profiling() {
   CFileStream file_stream("trace.json", "w");
   file_stream << "{\"traceEvents\":[\n";
   bool first(true);
-  for(uint32_t i(0); i<event_index; i++) {
+  for(uint32_t i(0); i < event_index; i++) {
     const ProfilingEvent& event(events[i]);
     const String escaped_event_name(String(event.name) // Need some escaping to put into json
-                                    .replaceAll("\n", "\\n")
-                                    .replaceAll("\"", "\\\""));
+      .replaceAll("\n", "\\n")
+      .replaceAll("\"", "\\\""));
     file_stream
       << (first ? ' ' : ',')
       << "{\"ph\":\"X\",\"tid\": " << event.fiber_id << ",\"pid\":" << 0 << ",\"name\":\"" << escaped_event_name
@@ -74,12 +68,12 @@ void L::flush_profiling() {
     first = false;
   }
 
-  for(uint32_t i(0); i<counter_event_index; i++) {
+  for(uint32_t i(0); i < counter_event_index; i++) {
     const ProfilingCounterEvent& event(counter_events[i]);
     const String escaped_event_name(String(event.name) // Need some escaping to put into json
       .replaceAll("\n", "\\n")
       .replaceAll("\"", "\\\""));
-    if(event.time.microseconds()>0) {
+    if(event.time.microseconds() > 0) {
       file_stream
         << (first ? ' ' : ',')
         << "{\"ph\":\"C\",\"pid\":" << 0 << ",\"name\":\"" << escaped_event_name
