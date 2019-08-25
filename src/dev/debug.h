40c86a9fcaec969a18bdfb097e28ae1ec619f6a6
Lucien Catonnet <lutopia@hotmail.fr>
2019-08-25 20:48:21 +0200

    Asserts are no longer debug-only

--- src/dev/debug.h
+++ src/dev/debug.h
@@ -1,13 +1,7 @@
 #pragma once
 
-#ifdef L_DEBUG
-#define L_DEBUGONLY(...) {__VA_ARGS__}
-#else
-#define L_DEBUGONLY(...) {}
-#endif
-
-#define L_ASSERT(...) L_DEBUGONLY(if(!(__VA_ARGS__))L::error("Assertion failure: %s",#__VA_ARGS__);)
-#define L_ASSERT_MSG(exp,msg) L_DEBUGONLY(if(!(exp))L::error(msg);)
+#define L_ASSERT(...) { if(!(__VA_ARGS__)) L::error("Assertion failure: %s",#__VA_ARGS__); }
+#define L_ASSERT_MSG(exp, msg) { if(!(exp)) L::error(msg); }
 
 namespace L {
   void error(const char* msg, ...); // Fatal, dumps callstack
