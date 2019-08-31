22260032f9d7168ac715adc27cbcd3a7a2fab208
Lucien Catonnet <lutopia@hotmail.fr>
2019-08-31 14:43:15 +0200

    Added hash_combine

--- src/hash.h
+++ src/hash.h
@@ -2,8 +2,8 @@
 
 #include <cstdint> 
 
-namespace L{
-  inline uint32_t fnv1a(const char* str){
+namespace L {
+  inline uint32_t fnv1a(const char* str) {
     uint32_t wtr(2166136261);
     while(*str) {
       wtr ^= *str;
@@ -12,7 +12,7 @@ namespace L{
     }
     return wtr;
   }
-  inline uint32_t fnv1a(const char* data,size_t size){
+  inline uint32_t fnv1a(const char* data, size_t size) {
     uint32_t wtr(2166136261);
     while(size--) {
       wtr ^= *data;
@@ -21,10 +21,14 @@ namespace L{
     }
     return wtr;
   }
-  constexpr uint32_t FNV1A(const char* str,uint32_t r){ return (*str) ? FNV1A(str+1,(r^*str)*16777619) : r; }
-  constexpr uint32_t FNV1A(const char* str){ return FNV1A(str,2166136261); }
+  constexpr uint32_t FNV1A(const char* str, uint32_t r) { return (*str) ? FNV1A(str + 1, (r^*str) * 16777619) : r; }
+  constexpr uint32_t FNV1A(const char* str) { return FNV1A(str, 2166136261); }
 
-  template <class T> inline uint32_t hash(const T& v){ return fnv1a((const char*)&v,sizeof(v)); }
-  inline uint32_t hash(const char* str){ return fnv1a(str); }
-  inline uint32_t hash(uint32_t v) { return v+1; }
+  template <class T> inline uint32_t hash(const T& v) { return fnv1a((const char*)&v, sizeof(v)); }
+  inline uint32_t hash(const char* str) { return fnv1a(str); }
+  inline uint32_t hash(uint32_t v) { return v + 1; }
+
+  template <class T> inline void hash_combine(uint32_t& h, const T& v) {
+    h ^= hash(v) + 0x9e3779b9 + (h << 6) + (h >> 2);
+  }
 }
