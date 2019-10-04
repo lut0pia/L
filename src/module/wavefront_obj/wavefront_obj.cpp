5540fc5c8bfdc8175f58e20b5ba05c0d867a6464
Lucien Catonnet <lutopia@hotmail.fr>
2019-10-04 18:15:29 +0200

    Wavefront obj module no longer relies on MeshBuilder

--- src/module/wavefront_obj/wavefront_obj.cpp
+++ src/module/wavefront_obj/wavefront_obj.cpp
@@ -2,19 +2,18 @@
 #include <L/src/container/Buffer.h>
 #include <L/src/engine/Resource.inl>
 #include <L/src/rendering/Mesh.h>
-#include <L/src/pipeline/MeshBuilder.h>
 
 using namespace L;
 
 static const Symbol obj_symbol("obj");
 
 static inline void ignore_line(const char*& c) {
-  while(*c!='\n' && *c!='\r') c++;
-  while(*c=='\n' || *c=='\r') c++;
+  while(*c != '\n' && *c != '\r') c++;
+  while(*c == '\n' || *c == '\r') c++;
 }
 static inline void read_scalars(const char*& c, Vector3f& scalars) {
-  for(uintptr_t i(0); i<3; i++) {
-    while(*c==' ') c++;
+  for(uintptr_t i(0); i < 3; i++) {
+    while(*c == ' ') c++;
     scalars[i] = float(atof(c));
     while(!Stream::isspace(*c)) c++;
   }
@@ -22,41 +21,35 @@ static inline void read_scalars(const char*& c, Vector3f& scalars) {
 static inline bool read_indices(const char*& c, Vector3i& indices) {
   indices = 0;
   while(Stream::isspace(*c)) c++;
-  for(uintptr_t i(0); i<3; i++) {
+  for(uintptr_t i(0); i < 3; i++) {
     indices[i] = atoi(c);
     while(isdigit(*c)) c++;
-    while(*c=='/') c++;
+    while(*c == '/') c++;
     if(Stream::isspace(*c))
       break;
   }
-  return indices.x()>0;
+  return indices.x() > 0;
+}
+template <size_t d>
+static inline void add_attribute(Array<float>& vertex, const Vector<d, float>& attr) {
+  for(uintptr_t i = 0; i < d; i++) {
+    vertex.push(attr[i]);
+  }
 }
 bool obj_loader(ResourceSlot& slot, Mesh::Intermediate& intermediate) {
   if(slot.ext != obj_symbol) {
     return false;
   }
 
-  struct Vertex {
-    Vector3f position;
-    Vector2f uv;
-    Vector3f normal;
-  };
-  MeshBuilder mesh_builder(sizeof(Vertex));
-  mesh_builder.position_offset = 0;
-  mesh_builder.uv_offset = sizeof(Vector2f) + sizeof(Vector3f);
-  mesh_builder.normal_offset = sizeof(Vector2f) + sizeof(Vector3f);
-
   Buffer buffer(slot.read_source_file());
-  const void* data(buffer.data());
-  const size_t size(buffer.size());
+  Array<float> vertices;
 
   Array<Vector3f> positions, normals;
-  Array<Vector2f> uvs;
-  Vertex vertex {}, firstVertex {}, lastVertex {};
+  Array<Vector2f> texcoords;
   Vector3f scalars;
   Vector3i indices;
-  const char *cur((const char*)data), *end(cur+size);
-  while(cur<end) {
+  const char *cur((const char*)buffer.data()), *end(cur + buffer.size());
+  while(cur < end) {
     switch(*cur) { // Line start
       case 'v':
         cur += 1;
@@ -70,7 +63,7 @@ bool obj_loader(ResourceSlot& slot, Mesh::Intermediate& intermediate) {
             cur += 1;
             read_scalars(cur, scalars);
             ignore_line(cur);
-            uvs.push(scalars.x(), 1.f-scalars.y());
+            texcoords.push(scalars.x(), 1.f - scalars.y());
             break;
           case 'n': // Normal
             cur += 1;
@@ -82,38 +75,44 @@ bool obj_loader(ResourceSlot& slot, Mesh::Intermediate& intermediate) {
         }
         break;
       case 'f':
+      {
         cur += 1;
-        for(uintptr_t i(0); read_indices(cur, indices); i++) {
-          if(indices.z()>0) vertex.normal = normals[indices.z()-1];
-          if(indices.y()>0) {
-            if(!uvs.empty()) vertex.uv = uvs[indices.y()-1];
-            else vertex.normal = normals[indices.y()-1];
-          }
-          vertex.position = positions[indices.x()-1];
 
-          if(i==0) firstVertex = vertex;
-          else if(i>2) {
-            mesh_builder.add_vertex(&firstVertex);
-            mesh_builder.add_vertex(&lastVertex);
+        Array<float> first_vertex, last_vertex;
+        for(uintptr_t i(0); read_indices(cur, indices); i++) {
+          Array<float> vertex;
+          add_attribute(vertex, positions[indices.x() - 1]);
+          if(indices.y() > 0 && texcoords.size() >= indices.y()) {
+            add_attribute(vertex, texcoords[indices.y() - 1]);
           }
-          mesh_builder.add_vertex(&vertex);
-          lastVertex = vertex;
+          if(indices.z() > 0 && normals.size() >= indices.z()) {
+            add_attribute(vertex, normals[indices.z() - 1]);
+          }
+
+          if(i == 0) first_vertex = vertex;
+          else if(i > 2) {
+            vertices += first_vertex;
+            vertices += last_vertex;
+          }
+          vertices += vertex;
+          last_vertex = vertex;
         }
         break;
+      }
       default: ignore_line(cur); break;
     }
   }
 
-  if(normals.empty())
-    mesh_builder.compute_normals();
-
-  intermediate.vertices = Buffer(mesh_builder.vertices(), mesh_builder.vertices_size());
-  intermediate.indices = Buffer(mesh_builder.indices(), mesh_builder.index_count() * sizeof(uint16_t));
-  intermediate.attributes = {
-    {VK_FORMAT_R32G32B32_SFLOAT, MeshAttributeType::Position},
-    {VK_FORMAT_R32G32_SFLOAT, MeshAttributeType::TexCoord},
-    {VK_FORMAT_R32G32B32_SFLOAT, MeshAttributeType::Normal},
-  };
+  intermediate.vertices = Buffer(vertices.begin(), vertices.size() * sizeof(float));
+  intermediate.indices = Buffer();
+  intermediate.attributes.clear();
+  intermediate.attributes.push(MeshAttribute {VK_FORMAT_R32G32B32_SFLOAT, MeshAttributeType::Position});
+  if(!texcoords.empty()) {
+    intermediate.attributes.push(MeshAttribute {VK_FORMAT_R32G32_SFLOAT, MeshAttributeType::TexCoord});
+  }
+  if(!normals.empty()) {
+    intermediate.attributes.push(MeshAttribute {VK_FORMAT_R32G32B32_SFLOAT, MeshAttributeType::Normal});
+  }
   return true;
 }
 
