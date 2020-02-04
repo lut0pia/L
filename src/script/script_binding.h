2f74eca342b4cd34b763f1ad5b4b33cfd5459762
Lucien Catonnet <lutopia@hotmail.fr>
2020-02-04 22:01:15 +0100

    Script binding macro checks for invalid handles

--- src/script/script_binding.h
+++ src/script/script_binding.h
@@ -2,33 +2,24 @@
 
 #include "ScriptContext.h"
 
-#define L_SCRIPT_METHOD(TYPE, NAME, NARGS, ...) \
+#define L_SCRIPT_METHOD_INTERNAL(PREFIX, TYPE, NAME, NARGS, ...) \
   ScriptContext::type_value(Type<TYPE*>::description(), Symbol(NAME)) = \
   ScriptContext::type_value(Type<Handle<TYPE>>::description(), Symbol(NAME)) = \
   (ScriptNativeFunction)([](ScriptContext& c) { \
     if(c.param_count() < NARGS) { \
       warning("Too few arguments in method call '%s.%s'", #TYPE, NAME); \
     } \
-    if(c.current_self().is<TYPE*>()) { \
-      c.current_self().as<TYPE*>()->__VA_ARGS__; \
-    } else if(c.current_self().is<Handle<TYPE>>()) { \
-      c.current_self().as<Handle<TYPE>>()->__VA_ARGS__; \
+    if(TYPE** ptr = c.current_self().try_as<TYPE*>()) { \
+      PREFIX (*ptr)->__VA_ARGS__; \
+    } else if(Handle<TYPE>* handle = c.current_self().try_as<Handle<TYPE>>()) { \
+      if(handle->is_valid()) { \
+        PREFIX (*handle)->__VA_ARGS__; \
+      } else { \
+        warning("Calling method '%s.%s' on invalid handle", #TYPE, NAME); \
+      } \
     } else { \
       warning("Calling method '%s.%s' on wrong type '%s'", #TYPE, NAME, c.current_self().type()->name); \
     } \
   })
-#define L_SCRIPT_RETURN_METHOD(TYPE, NAME, NARGS, ...) \
-  ScriptContext::type_value(Type<TYPE*>::description(), Symbol(NAME)) = \
-  ScriptContext::type_value(Type<Handle<TYPE>>::description(), Symbol(NAME)) = \
-  (ScriptNativeFunction)([](ScriptContext& c) { \
-    if(c.param_count() < NARGS) { \
-      warning("Too few arguments in method call '%s.%s'", #TYPE, NAME); \
-    } \
-    if(c.current_self().is<TYPE*>()) { \
-      c.return_value() = c.current_self().as<TYPE*>()->__VA_ARGS__; \
-    } else if(c.current_self().is<Handle<TYPE>>()) { \
-      c.return_value() = c.current_self().as<Handle<TYPE>>()->__VA_ARGS__; \
-    } else { \
-      warning("Calling method '%s.%s' on wrong type '%s'", #TYPE, NAME, c.current_self().type()->name); \
-    } \
-  })
+#define L_SCRIPT_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(, TYPE, NAME, NARGS, __VA_ARGS__)
+#define L_SCRIPT_RETURN_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(c.return_value() =, TYPE, NAME, NARGS, __VA_ARGS__)
