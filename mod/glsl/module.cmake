49c176e26728c74662435d2f3eb131cce1d8d93c
Lucien Catonnet <lutopia@hotmail.fr>
2020-02-08 22:59:01 +0100

    CMake: Specify build type via variable for ext_glslang

--- mod/glsl/module.cmake
+++ mod/glsl/module.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(
   GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
   GIT_TAG 8db9eccc0baf30c9d22c496ab28db0fe1e4e97c5 # v8.13.3559
   GIT_SHALLOW true
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GLSLANG_INSTALL_DIR}
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GLSLANG_INSTALL_DIR} -DCMAKE_BUILD_TYPE=$<IF:$<CONFIG:Debug>,DEBUG,RELEASE>
   BUILD_COMMAND ${CMAKE_COMMAND} --build . --config $<IF:$<CONFIG:Debug>,Debug,Release>
   INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install --config $<IF:$<CONFIG:Debug>,Debug,Release>
 )
