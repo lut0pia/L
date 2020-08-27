cb5d37b1ed5451094576d04165a5908f6df3111e
Lucien Catonnet <lutopia@hotmail.fr>
2020-08-27 18:26:49 +0200

    Added 64bit variants of cas and atomic_add

--- src/system/intrinsics.h
+++ src/system/intrinsics.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cstdint> 
+#include <cstdint>
 
 #if _MSC_VER
 #include <intrin.h>
@@ -26,7 +26,7 @@ namespace L {
     return sizeof(v)*8-__builtin_clzll(v);
 #endif
   }
-  
+
   inline uint32_t cas(volatile uint32_t* dst, uint32_t cmp, uint32_t exc) {
 #if _MSC_VER
     return _InterlockedCompareExchange((volatile long*)dst, exc, cmp);
@@ -35,11 +35,27 @@ namespace L {
 #endif
   }
 
-  inline void atomic_add(volatile int32_t* counter, int32_t value) {
+  inline uint64_t cas(volatile uint64_t* dst, uint64_t cmp, uint64_t exc) {
 #if _MSC_VER
-    _InterlockedExchangeAdd((volatile long*)counter, value);
+    return _InterlockedCompareExchange((volatile long*)dst, exc, cmp);
+#elif __GNUC__
+    return __sync_val_compare_and_swap(dst, cmp, exc);
+#endif
+  }
+
+  inline int32_t atomic_add(volatile int32_t* counter, int32_t value) {
+#if _MSC_VER
+    return _InterlockedExchangeAdd((volatile long*)counter, value);
 #else
-    __sync_fetch_and_add(counter, value);
+    return __sync_fetch_and_add(counter, value);
+#endif
+  }
+
+  inline int64_t atomic_add(volatile int64_t* counter, int64_t value) {
+#if _MSC_VER
+    return _InterlockedExchangeAdd64((counter, value);
+#else
+    return __sync_fetch_and_add(counter, value);
 #endif
   }
 }
