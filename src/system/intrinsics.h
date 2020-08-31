e058db4a5318523db5181c835e88a0eeb28fe49a
Lucien Catonnet <lutopia@hotmail.fr>
2020-08-31 15:26:33 +0200

    Fixed 64bit intrinsics for MSVC

--- src/system/intrinsics.h
+++ src/system/intrinsics.h
@@ -37,7 +37,7 @@ namespace L {
 
   inline uint64_t cas(volatile uint64_t* dst, uint64_t cmp, uint64_t exc) {
 #if _MSC_VER
-    return _InterlockedCompareExchange((volatile long*)dst, exc, cmp);
+    return _InterlockedCompareExchange64((volatile long long*)dst, exc, cmp);
 #elif __GNUC__
     return __sync_val_compare_and_swap(dst, cmp, exc);
 #endif
@@ -53,7 +53,7 @@ namespace L {
 
   inline int64_t atomic_add(volatile int64_t* counter, int64_t value) {
 #if _MSC_VER
-    return _InterlockedExchangeAdd64((counter, value);
+    return _InterlockedExchangeAdd64(counter, value);
 #else
     return __sync_fetch_and_add(counter, value);
 #endif
