aee9a37cec40c1a355d381af14a0aec1c4aa1d39
Lucien Catonnet <lutopia@hotmail.fr>
2020-11-15 14:18:38 +0000

    xinput_unix: Check for availability before registering module

--- mod/xinput_unix/module.cmake
+++ mod/xinput_unix/module.cmake
@@ -1,3 +1,9 @@
+find_path(XINPUT2_PATHS XInput2.h)
+if(NOT XINPUT2_PATHS)
+  message("-- Could NOT find XInput2")
+  return()
+endif()
+
 add_module(
   xinput_unix
   CONDITION $<PLATFORM_ID:Linux>
