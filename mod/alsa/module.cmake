95b7fe96e2b57d46d2276086b750a20167697268
Lucien Catonnet <lutopia@hotmail.fr>
2020-11-18 11:18:23 +0100

    CMake: Unset find_path cache output

--- mod/alsa/module.cmake
+++ mod/alsa/module.cmake
@@ -1,7 +1,10 @@
 find_path(ALSA_PATHS alsa/asoundlib.h)
 if(NOT ALSA_PATHS)
-  message("-- Could NOT find ALSA")
+  message("Could NOT find ALSA")
+  unset(ALSA_PATHS CACHE)
   return()
+else()
+  unset(ALSA_PATHS CACHE)
 endif()
 
 add_module(
--- mod/xinput_unix/module.cmake
+++ mod/xinput_unix/module.cmake
@@ -1,7 +1,10 @@
 find_path(XINPUT2_PATHS X11/extensions/XInput2.h)
 if(NOT XINPUT2_PATHS)
-  message("-- Could NOT find XInput2")
+  message("Could NOT find XInput2")
+  unset(XINPUT2_PATHS CACHE)
   return()
+else()
+  unset(XINPUT2_PATHS CACHE)
 endif()
 
 add_module(
