7064a546223452269ca37ad15e775638155ddc4a
Lucien Catonnet <lutopia@hotmail.fr>
2020-12-24 17:08:50 +0100

    Fixed literal concatenation mistaken for user-defined literal

--- src/dev/debug.h
+++ src/dev/debug.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #define L_ASSERT(...) { if(!(__VA_ARGS__)) L::error("Assertion failure: %s",#__VA_ARGS__); }
-#define L_ASSERT_MSG(exp, ...) { if(!(exp)) L::error("Assertion failure: "__VA_ARGS__); }
+#define L_ASSERT_MSG(exp, ...) { if(!(exp)) L::error("Assertion failure: " __VA_ARGS__); }
 
 namespace L {
   void init_log_file();
