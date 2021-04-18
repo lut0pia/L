2104b36a0d543f22304b9efa660166d18de36499
Lucien Catonnet <lutopia@hotmail.fr>
2021-04-18 18:25:32 +0200

    Added out-of-bounds check for debug allocator on Windows

--- src/system/Memory_win.cpp
+++ src/system/Memory_win.cpp
@@ -5,8 +5,19 @@
 using namespace L;
 
 void* Memory::virtual_alloc(size_t size) {
-  return VirtualAlloc(0, size, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+  void* ptr = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+#if L_USE_DEBUG_ALLOC
+  SYSTEM_INFO si;
+  GetSystemInfo(&si);
+  // Put the allocation at the end of the page boundary
+  const size_t remainder = size % si.dwPageSize;
+  ptr = (uint8_t*)ptr + (remainder ? si.dwPageSize - remainder : 0);
+  // Allocate a "no access" page right after the allocation
+  VirtualAlloc((uint8_t*)ptr + size, si.dwPageSize, MEM_RESERVE, PAGE_NOACCESS);
+#endif
+  return ptr;
 }
 void Memory::virtual_free(void* ptr, size_t) {
-  VirtualFree(ptr, 0, MEM_RELEASE);
+  BOOL success = VirtualFree(ptr, 0, MEM_RELEASE);
+  L_ASSERT(success);
 }
