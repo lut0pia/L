d440a11337c992d5f0fe72411bcd0b8d03cbe5e2
Lucien Catonnet <lutopia@hotmail.fr>
2021-04-18 18:04:48 +0200

    opengl: Fixed dereferencing wrong type

--- mod/opengl/OpenGLUniformBuffer.cpp
+++ mod/opengl/OpenGLUniformBuffer.cpp
@@ -11,13 +11,13 @@ L::UniformBufferImpl* OpenGLRenderer::create_uniform_buffer(size_t size) {
   return uniform_buffer;
 }
 void OpenGLRenderer::destroy_uniform_buffer(UniformBufferImpl* uniform_buffer) {
-  OpenGLTexture* gl_uniform_buffer = (OpenGLTexture*)uniform_buffer;
+  OpenGLUniformBuffer* gl_uniform_buffer = (OpenGLUniformBuffer*)uniform_buffer;
   glDeleteBuffers(1, &gl_uniform_buffer->id);
   Memory::delete_type(gl_uniform_buffer);
 }
 void OpenGLRenderer::load_uniform_buffer(UniformBufferImpl* uniform_buffer, const void* data, size_t size, size_t offset) {
   L_SCOPE_THREAD_MASK(1);
 
-  OpenGLTexture* gl_uniform_buffer = (OpenGLTexture*)uniform_buffer;
+  OpenGLUniformBuffer* gl_uniform_buffer = (OpenGLUniformBuffer*)uniform_buffer;
   glNamedBufferSubData(gl_uniform_buffer->id, offset, size, data);
 }
