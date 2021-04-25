006e309c110986d75acb2158dfa01c856d7b340c
Lucien Catonnet <lutopia@hotmail.fr>
2021-04-25 23:56:18 +0200

    Allow binding script to Refs
    Also allow wrapping return values to translate them

--- src/script/script_binding.h
+++ src/script/script_binding.h
@@ -11,24 +11,30 @@
     __VA_ARGS__; \
   })
 
-#define L_SCRIPT_METHOD_INTERNAL(PREFIX, TYPE, NAME, NARGS, ...) \
-  register_script_method<Handle<TYPE>>(NAME, \
+#define L_SCRIPT_METHOD_INTERNAL(METATYPE, PREFIX, TYPE, NAME, NARGS, ...) \
+  register_script_method<METATYPE<TYPE>>(NAME, \
   [](ScriptContext& c) { \
     if(c.param_count() < NARGS) { \
       c.warning("Too few arguments in method call '%s.%s'", #TYPE, NAME); \
     } \
-    if(Handle<TYPE>* handle = c.current_self().try_as<Handle<TYPE>>()) { \
-      if(handle->is_valid()) { \
-        PREFIX (*handle)->__VA_ARGS__; \
+    if(METATYPE<TYPE>* meta = c.current_self().try_as<METATYPE<TYPE>>()) { \
+      if(meta->is_valid()) { \
+        PREFIX ((*meta)->__VA_ARGS__); \
       } else { \
-        c.warning("Calling method '%s.%s' on invalid handle", #TYPE, NAME); \
+        c.warning("Calling method '%s.%s' on invalid reference", #TYPE, NAME); \
       } \
     } else { \
       c.warning("Calling method '%s.%s' on wrong type '%s'", #TYPE, NAME, c.current_self().type()->name); \
     } \
   })
-#define L_SCRIPT_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(, TYPE, NAME, NARGS, __VA_ARGS__)
-#define L_SCRIPT_RETURN_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(c.return_value() =, TYPE, NAME, NARGS, __VA_ARGS__)
+
+#define L_SCRIPT_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(Handle, , TYPE, NAME, NARGS, __VA_ARGS__)
+#define L_SCRIPT_RETURN_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(Handle, c.return_value() =, TYPE, NAME, NARGS, __VA_ARGS__)
+#define L_SCRIPT_WRAPPED_RETURN_METHOD(TYPE, NAME, NARGS, WRAPPER, ...) L_SCRIPT_METHOD_INTERNAL(Handle, c.return_value() = WRAPPER, TYPE, NAME, NARGS, __VA_ARGS__)
+
+#define L_SCRIPT_REF_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(Ref, , TYPE, NAME, NARGS, __VA_ARGS__)
+#define L_SCRIPT_REF_RETURN_METHOD(TYPE, NAME, NARGS, ...) L_SCRIPT_METHOD_INTERNAL(Ref, c.return_value() =, TYPE, NAME, NARGS, __VA_ARGS__)
+#define L_SCRIPT_REF_WRAPPED_RETURN_METHOD(TYPE, NAME, NARGS, WRAPPER, ...) L_SCRIPT_METHOD_INTERNAL(Ref, c.return_value() = WRAPPER, TYPE, NAME, NARGS, __VA_ARGS__)
 
 namespace L {
   template <class T>
