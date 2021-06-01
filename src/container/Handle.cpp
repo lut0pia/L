896944833d8e296a23d449f1f9bf4de5ea5d6203
Lucien Catonnet <lutopia@hotmail.fr>
2021-06-01 21:26:35 +0200

    Improved handle code readability

--- src/container/Handle.cpp
+++ src/container/Handle.cpp
@@ -1,27 +1,33 @@
 #include "Handle.h"
 
-#include "../system/intrinsics.h"
 #include "../system/Memory.h"
+#include "../system/intrinsics.h"
 
 using namespace L;
 
+static constexpr uint32_t address_bits = 48;
+static constexpr uint64_t address_mask = (1ull << address_bits) - 1;
+static constexpr uint64_t version_bits = 16;
+static constexpr uint64_t version_mask = (1ull << version_bits) - 1;
+
 static uint64_t* handle_objects = nullptr;
 static int64_t next_index = 0;
-static uint32_t address_bits = 48;
-static uint64_t address_mask = (1ull << address_bits) - 1;
 static uint64_t freelist = address_mask;
 
 GenericHandle::GenericHandle(void* ptr) {
   L_ASSERT((uint64_t(ptr) & address_mask) == uint64_t(ptr));
 
   while(freelist != address_mask) {
-    const uint64_t index = freelist;
-    const uint64_t version = handle_objects[index] >> address_bits;
-    _ver_index = (version << address_bits) | index;
-    if(cas(&freelist, index, handle_objects[index] & address_mask) != index) {
+    const uint64_t freeslot_index = freelist;
+    const uint64_t freeslot = handle_objects[freeslot_index];
+    const uint64_t version = freeslot >> address_bits;
+    const uint64_t new_freeslot_index = freeslot & address_mask;
+
+    _ver_index = (version << address_bits) | freeslot_index;
+    if(cas(&freelist, freeslot_index, new_freeslot_index) != freeslot_index) {
       continue;
     }
-    handle_objects[index] = (version << address_bits) | (uint64_t)ptr;
+    handle_objects[freeslot_index] = (version << address_bits) | (uint64_t)ptr;
     return;
   }
 
@@ -55,15 +61,15 @@ uint64_t GenericHandle::index() const {
 void GenericHandle::release() {
   if(_ver_index != UINT64_MAX) {
     const uint64_t version = _ver_index >> address_bits;
-    const uint64_t new_free = _ver_index & address_mask;
-    const uint64_t object = handle_objects[new_free];
+    const uint64_t new_freeslot_index = _ver_index & address_mask;
+    const uint64_t new_freeslot = handle_objects[new_freeslot_index];
 
-    if((object >> address_bits) == version) {
-      uint64_t old_free;
+    if((new_freeslot >> address_bits) == version) {
+      uint64_t old_freeslot;
       do {
-        old_free = freelist;
-        handle_objects[new_free] = ((version + 1) << address_bits) | old_free;
-      } while(cas(&freelist, old_free, new_free) != old_free);
+        old_freeslot = freelist;
+        handle_objects[new_freeslot_index] = ((version + 1) << address_bits) | old_freeslot;
+      } while(cas(&freelist, old_freeslot, new_freeslot_index) != old_freeslot);
     }
 
     _ver_index = UINT64_MAX;
