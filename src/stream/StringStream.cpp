632aca364e80b2ecf1659a3f15048aa752af8a0d
Lucien Catonnet <lutopia@hotmail.fr>
2021-06-18 23:18:37 +0200

    Made StringStream a DirectStream

--- src/stream/StringStream.cpp
+++ src/stream/StringStream.cpp
@@ -3,17 +3,17 @@
 using namespace L;
 
 size_t StringStream::write(const void* data, size_t size) {
-  const uintptr_t prev_size(_string.size());
-  _string.size(_string.size()+size);
-  memcpy(_string+prev_size, data, size);
+  const uintptr_t prev_size = _string.size();
+  _string.size(_string.size() + size);
+  memcpy(_string + prev_size, data, size);
   return size;
 }
 size_t StringStream::read(void* data, size_t size) {
-  size = min(size, _string.size()-_index);
+  size = min(size, _string.size() - _index);
   if(size) {
-    memcpy(data, _string.begin()+_index, size);
+    memcpy(data, _string.begin() + _index, size);
     _index += size;
   }
-  _index += (_index==_string.size()); // Indicate end of stream
+  _index += (_index == _string.size()); // Indicate end of stream
   return size;
 }
--- src/stream/StringStream.h
+++ src/stream/StringStream.h
@@ -1,16 +1,23 @@
 #pragma once
 
-#include "Stream.h"
 #include "../text/String.h"
+#include "DirectStream.h"
 
 namespace L {
-  class StringStream : public Stream {
+  class StringStream : public DirectStream {
   protected:
     String _string;
     uintptr_t _index = 0;
+
   public:
-    size_t write(const void*, size_t) override;
-    size_t read(void*, size_t) override;
+    virtual size_t write(const void*, size_t) override;
+    virtual size_t read(void*, size_t) override;
+
+    virtual char peek() override { return _string[_index]; }
+    virtual void seek(uintptr_t index) override { _index = index; }
+    virtual uintptr_t tell() override { return _index; }
+    virtual size_t size() override { return _string.size(); }
+
     const String& string() const { return _string; }
   };
   template <class T>
