fa79ec63e1e22269f7c477cbf7e4933547986c09
Lucien Catonnet <lutopia@hotmail.fr>
2021-06-23 12:40:55 +0200

    Removed cas and atomic_add
    Using <atomic> instead from now on

--- src/system/intrinsics.h
+++ src/system/intrinsics.h
@@ -26,36 +26,4 @@ namespace L {
     return sizeof(v)*8-__builtin_clzll(v);
 #endif
   }
-
-  inline uint32_t cas(volatile uint32_t* dst, uint32_t cmp, uint32_t exc) {
-#if _MSC_VER
-    return _InterlockedCompareExchange((volatile long*)dst, exc, cmp);
-#elif __GNUC__
-    return __sync_val_compare_and_swap(dst, cmp, exc);
-#endif
-  }
-
-  inline uint64_t cas(volatile uint64_t* dst, uint64_t cmp, uint64_t exc) {
-#if _MSC_VER
-    return _InterlockedCompareExchange64((volatile long long*)dst, exc, cmp);
-#elif __GNUC__
-    return __sync_val_compare_and_swap(dst, cmp, exc);
-#endif
-  }
-
-  inline int32_t atomic_add(volatile int32_t* counter, int32_t value) {
-#if _MSC_VER
-    return _InterlockedExchangeAdd((volatile long*)counter, value);
-#else
-    return __sync_fetch_and_add(counter, value);
-#endif
-  }
-
-  inline int64_t atomic_add(volatile int64_t* counter, int64_t value) {
-#if _MSC_VER
-    return _InterlockedExchangeAdd64(counter, value);
-#else
-    return __sync_fetch_and_add(counter, value);
-#endif
-  }
 }
