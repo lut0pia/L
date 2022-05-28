5f291ece41795ca4d0a8376962055ce108315226
Lutopia <lutopia@hotmail.fr>
2022-05-28 20:20:26 +0200

    imgui_resources: Using table API
    Support for sorting

--- mod/imgui_resources/imgui_resources.cpp
+++ mod/imgui_resources/imgui_resources.cpp
@@ -36,7 +36,7 @@ static void imgui_resources_update() {
 
   Array<ResourceSlot*> slots = ResourceSlot::slots();
 
-  if(ImGui::BeginPopup("State Filter")) {
+  if(ImGui::BeginPopup("State Filter Popup")) {
     ImGui::CheckboxFlags("Unloaded", &state_mask, 1 << uint32_t(ResourceState::Unloaded));
     ImGui::CheckboxFlags("Loading", &state_mask, 1 << uint32_t(ResourceState::Loading));
     ImGui::CheckboxFlags("Loaded", &state_mask, 1 << uint32_t(ResourceState::Loaded));
@@ -44,39 +44,59 @@ static void imgui_resources_update() {
     ImGui::EndPopup();
   }
 
-  ImGui::Columns(5, "Columns");
-
-  ImGui::SetColumnWidth(0, 128);
-  ImGui::SetColumnWidth(1, max(16.f, ImGui::GetWindowContentRegionWidth() - 128 - 64 - 64 - 64));
-  ImGui::SetColumnWidth(2, 64);
-  ImGui::SetColumnWidth(3, 64);
-  ImGui::SetColumnWidth(4, 64);
-
-  ImGui::Separator();
-
-  ImGui::Text("Type");
-  ImGui::NextColumn();
-
-  ImGui::Text("Id");
-  ImGui::NextColumn();
-
-  ImGui::Text("CPU");
-  ImGui::NextColumn();
-
-  ImGui::Text("GPU");
-  ImGui::NextColumn();
-
-  if(ImGui::Button("State")) {
-    ImGui::OpenPopup("State Filter");
+  if(ImGui::Button("State Filter")) {
+    ImGui::OpenPopup("State Filter Popup");
   }
-  ImGui::NextColumn();
 
-  ImGui::Separator();
+  if(!ImGui::BeginTable("Columns", 5, ImGuiTableFlags_Borders | ImGuiTableFlags_ScrollY | ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_Sortable)) {
+    ImGui::End();
+    return;
+  }
+
+  ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, 100);
+  ImGui::TableSetupColumn("Id", ImGuiTableColumnFlags_WidthStretch);
+  ImGui::TableSetupColumn("CPU", ImGuiTableColumnFlags_WidthFixed, 50);
+  ImGui::TableSetupColumn("GPU", ImGuiTableColumnFlags_WidthFixed, 50);
+  ImGui::TableSetupColumn("State", ImGuiTableColumnFlags_WidthFixed, 60);
+  ImGui::TableSetupScrollFreeze(0, 1);
+  ImGui::TableHeadersRow();
+
+  // Sort slots according to ImGui sort specs
+  static ImGuiTableSortSpecs* sort_specs;
+  sort_specs = ImGui::TableGetSortSpecs();
+  if(sort_specs != nullptr) {
+    qsort(slots.begin(), slots.size(), sizeof(slots[0]),
+      [](void const* lhs, void const* rhs) -> int {
+        const ResourceSlot* a = *(const ResourceSlot**)lhs;
+        const ResourceSlot* b = *(const ResourceSlot**)rhs;
+        for(int n = 0; n < sort_specs->SpecsCount; n++) {
+          const ImGuiTableColumnSortSpecs* sort_spec = &sort_specs->Specs[n];
+          int delta = 0;
+          switch(sort_spec->ColumnIndex) {
+            case 0: delta = strcmp(a->type, b->type); break;
+            case 1: delta = strcmp(a->id, b->id); break; // TODO: Should be case-insensitive
+            case 2: delta = static_cast<int>(a->cpu_size - b->cpu_size); break;
+            case 3: delta = static_cast<int>(a->gpu_size - b->gpu_size); break;
+            case 4: delta = static_cast<int>(a->state.load()) - static_cast<int>(b->state.load()); break;
+            default: IM_ASSERT(0); break;
+          }
+          if(delta > 0) {
+            return (sort_spec->SortDirection == ImGuiSortDirection_Ascending) ? +1 : -1;
+          } else if(delta < 0) {
+            return (sort_spec->SortDirection == ImGuiSortDirection_Ascending) ? -1 : +1;
+          }
+        }
+
+        return static_cast<int>(a - b);
+      });
+  }
 
   for(ResourceSlot* slot : slots) {
     if(((1 << uint32_t(slot->state.load())) & state_mask) == 0) {
       continue;
     }
+
+    ImGui::TableNextRow();
 
     bool type_color_created;
     Color* type_color = type_colors.find_or_create(slot->type, &type_color_created);
@@ -106,30 +126,31 @@ static void imgui_resources_update() {
     }
 
     String type = String(slot->type).replace_all("L::", "");
+    ImGui::TableSetColumnIndex(0);
     ImGui::TextColored(im_type_color, "%s", type.begin());
-    ImGui::NextColumn();
 
+    ImGui::TableSetColumnIndex(1);
     ImGui::Text("%s", (const char*)slot->id);
-    ImGui::NextColumn();
 
+    ImGui::TableSetColumnIndex(2);
     if(slot->cpu_size > 0) {
-      String cpu_size = format_memory_amount(slot->cpu_size);
-      ImGui::SetCursorPosX(ImGui::GetCursorPosX() + ImGui::GetColumnWidth() - ImGui::CalcTextSize(cpu_size.begin()).x - ImGui::GetScrollX() - 2 * ImGui::GetStyle().ItemSpacing.x);
+      const String cpu_size = format_memory_amount(slot->cpu_size);
+      ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 50 - ImGui::CalcTextSize(cpu_size.begin()).x);
       ImGui::Text("%s", cpu_size.begin());
     }
-    ImGui::NextColumn();
 
+    ImGui::TableSetColumnIndex(3);
     if(slot->gpu_size > 0) {
-      String gpu_size = format_memory_amount(slot->gpu_size);
-      ImGui::SetCursorPosX(ImGui::GetCursorPosX() + ImGui::GetColumnWidth() - ImGui::CalcTextSize(gpu_size.begin()).x - ImGui::GetScrollX() - 2 * ImGui::GetStyle().ItemSpacing.x);
-      ImGui::Text("%s", format_memory_amount(slot->gpu_size).begin());
+      const String gpu_size = format_memory_amount(slot->gpu_size);
+      ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 50 - ImGui::CalcTextSize(gpu_size.begin()).x);
+      ImGui::Text("%s", gpu_size.begin());
     }
-    ImGui::NextColumn();
 
+    ImGui::TableSetColumnIndex(4);
     ImGui::TextColored(state_color, "%s", state);
-    ImGui::NextColumn();
   }
-  ImGui::Columns(1);
+
+  ImGui::EndTable();
 
   ImGui::End();
 }
